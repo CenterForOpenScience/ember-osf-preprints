74455fb8be18852605f827a2821d9918457cb71e
Terron Ishihara <tishihara@email.arizona.edu>
2016-06-02 13:00:13 -0400

    Adjusted styling of recent uploads, colors (blue vs purple)

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,3 +4,8 @@ import OsfLoginRouteMixin from 'ember-osf/mixins/osf-login-route';
 export default Ember.Route.extend({OsfLoginRouteMixin
 });
 
+export default Ember.Route.extend({
+    model() {
+        return this.store.findAll('preprint');
+    }
+});
