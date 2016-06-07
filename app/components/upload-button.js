ec848e521d7b928d0f2a9bdd6d0d0d32b9d05437
Lauren Revere <geeksnglitter@gmail.com>
2016-06-07 15:50:20 -0400

    First semi-working upload modal

--- app/components/upload-button.js
+++ app/components/upload-button.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    showModal: false,
+    actions:{
+        submit(){
+            this.open('search');
+        },
+        modalShow(){
+            this.set('showModal', true);
+        },
+        modalHide(){
+            this.set('showModal', false);
+        },
+        openModal(){
+            this.set('showModal', false);
+        }
+    }
 });
