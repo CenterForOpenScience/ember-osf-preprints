839130fed999974bb945366fdd581e740ea22ec5
Lauren Revere <geeksnglitter@gmail.com>
2016-06-08 10:34:46 -0400

    Add **rough** draft of workflow modals

--- app/components/upload-button.js
+++ app/components/upload-button.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    showModal: false,
+    showModal: false, //TODO: Refactor obviously - there is OBVIOUSLY...a way better way to do tis...DRY!!
+    showLogin: false,
+    showSignup: false,
+    showProjectChooser: false,
+    showFileChooser: false,
+    showSignupSuccess: false,
     actions:{
         submit(){
             this.open('search');
@@ -9,11 +14,23 @@ export default Ember.Component.extend({
         modalShow(){
             this.set('showModal', true);
         },
-        modalHide(){
+        loginShow(){
+            this.set('showLogin', true);
+            this.set('showModal', false); //Once again... sigh should be able to move this switching to the HBS too...to the button click could just set all false--seems rough
+        },
+        signupShow(){
+            this.set('showSignup', true);
             this.set('showModal', false);
         },
-        openModal(){
-            this.set('showModal', false);
+        signupSuccessShow(){
+            this.set('showSignupSuccess', true);
+            this.set('showSignup', false);
+        },
+        projectsShow(){
+            this.set('showProjectChooser', true);
+        },
+        fileShow(){
+            this.set('showFileChooser', true);
         }
     }
 });
