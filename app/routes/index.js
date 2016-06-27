2cac78ce10e3f20b53a239def57510e8a2f9c9fa
Maxwell Luo <mustmax123@yahoo.com>
2016-06-27 16:37:10 -0400

    Remove scroll bar and add expand button

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,23 +1,31 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import $ from 'jquery';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {});
 
 export default Ember.Route.extend({
     fileManager: Ember.inject.service(),
     model() {
-    //MIRAGE
+        //MIRAGE
         return {
             preprints: this.store.findAll('preprint'),
             subjects: this.store.findAll('subject')
         };
 
-    //EMBER OSF
-    //return this.store.findAll('Node');
+        //EMBER OSF
+        //return this.store.findAll('Node');
     },
     actions: {
         goToSubject(sub, subID) {
             this.transitionTo('browse-preprints', {queryParams: {subject: sub, subjectID: subID}});
-        }
+        },
+        showPopularUploads() {
+            $('.show-more-button').click(function () {
+                var newHeight = ($('#landing-page-preprint-list').height() === 700) ? 2500 : 700;
+                $('#landing-page-preprint-list').css('max-height', newHeight);
+                $('#show-more-icon').toggleClass('fa fa-caret-down fa fa-caret-up');
+            });
+        },
     }
 });
