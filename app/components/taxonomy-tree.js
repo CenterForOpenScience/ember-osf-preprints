69c987fac7eb1fe38347ec6a71d06019e1fed182
Terron Ishihara <tishihara@email.arizona.edu>
2016-06-28 15:20:20 -0400

    Commented out calls that require JamDB

--- app/components/taxonomy-tree.js
+++ app/components/taxonomy-tree.js
@@ -4,6 +4,9 @@ import $ from 'jquery';
 export default Ember.Component.extend({
     store: Ember.inject.service(),
 
+    // JamDB
+    /*
+
     didInsertElement() {
         this.get('store').find('taxonomy', 'top3levels')
         .then(function(taxonomy) {
@@ -13,11 +16,11 @@ export default Ember.Component.extend({
                 selectedBackColor: '#67a3bf',
 
                 onNodeSelected: function(event, data) {
-                    if (data.text == 'All subjects') {
-                        this.sendAction('filter', null);
-                    } else {
-                        this.sendAction('filter', data.text);
-                    }
+//                    if (data.text == 'All subjects') {
+//                        this.sendAction('filter', null);
+//                    } else {
+//                        this.sendAction('filter', data.text);
+//                    }
 //                    // How to iterate over the child nodes of given node
 //                    for (var i=0; i<data.nodes.length; i++) {
 
@@ -26,5 +29,7 @@ export default Ember.Component.extend({
             });
         }.bind(this));
     }
+
+    */
 
 });
--- app/routes/browse.js
+++ app/routes/browse.js
@@ -8,7 +8,8 @@ export default Ember.Route.extend({
         }
     },
     model() {
-        return this.store.findAll('preprint');
+        // JamDB
+//        return this.store.findAll('preprint');
     },
     actions: {
         filter: function(subjectToFilter) {
