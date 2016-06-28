41228cf8c06dd6964d386fe942846bda02d26974
Maxwell Luo <mustmax123@yahoo.com>
2016-06-28 15:08:36 -0400

    Auto figure out dates and preprints but not contributors

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,9 +8,15 @@ export default Ember.Route.extend({
     fileManager: Ember.inject.service(),
     model() {
         //MIRAGE
+        var todaysDate = new Date();
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var formattedDate = days[todaysDate.getDay()] + " " + months[todaysDate.getMonth()] + " "
+        + todaysDate.getDate() + ", " + todaysDate.getFullYear();
         return {
             preprints: this.store.findAll('preprint'),
-            subjects: this.store.findAll('subject')
+            subjects: this.store.findAll('subject'),
+            theDate: formattedDate
         };
 
         //EMBER OSF
@@ -20,7 +26,7 @@ export default Ember.Route.extend({
         goToSubject(sub, subID) {
             this.transitionTo('browse-preprints', {queryParams: {subject: sub, subjectID: subID}});
         },
-        togglePopularUploads(   ) {
+        togglePopularUploads() {
             var newHeight = ($('#landing-page-preprint-list').height() === 700) ? 2500 : 700;
             $('#landing-page-preprint-list').css('max-height', newHeight);
             $('#show-more-icon').toggleClass('fa fa-caret-down fa fa-caret-up');
