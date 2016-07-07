692c861cf8e5ca68ea849067359df183bb41e853
Lauren Revere <geeksnglitter@gmail.com>
2016-07-07 16:07:30 -0400

    Fix authorization, hack fix remove toast as a service - add back in later

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,20 +1,20 @@
 import Ember from 'ember';
 
-import OsfLoginControllerMixin from 'ember-osf/mixins/osf-login-controller';
+import OsfLoginControllerMixin from 'ember-osf/mixins/osf-token-login-controller';
 
 import {
     getAuthUrl
 } from 'ember-osf/utils/auth';
 
 export default Ember.Controller.extend(OsfLoginControllerMixin, {
-    toast: Ember.inject.service(),
+    //toast: Ember.inject.service(),
     authUrl: getAuthUrl(),
     actions: {
         loginSuccess() {
             this.refresh();
         },
         loginFail(/* err */) {
-            this.get('toast').error('Login failed');
+            //this.get('toast').error('Login failed');
         }
     }
 });
