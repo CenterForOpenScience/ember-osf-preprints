c6a89a92805918d51f9e4aca013374f73541daa6
Ellis Tsung <git@tsung.co>
2016-07-25 14:40:29 -0400

    Preliminary upload section

--- app/components/preprint-form-section.js
+++ app/components/preprint-form-section.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import CpPanelComponent from 'ember-collapsible-panel/components/cp-panel';
 
 export default CpPanelComponent.extend({
-    tagName: 'section'
+    tagName: 'section',
+    handleToggle() {
+        // Prevent closing all views
+        if (!this.get('isOpen')) {
+            this._super(...arguments);
+        }
+    }
 });
--- app/components/preprint-form-upload.js
+++ app/components/preprint-form-upload.js
@@ -2,8 +2,27 @@ import Ember from 'ember';
 import CpPanelBodyComponent from 'ember-collapsible-panel/components/cp-panel-body';
 import PreprintFormFieldMixin from '../mixins/preprint-form-field';
 
-export default CpPanelBodyComponent.extend(PreprintFormFieldMixin, {
+// Enum of available states
+export const State = Ember.Object.extend(Ember.Freezable, {
+    START: 'start',
+    NEW: 'new',
+    EXISTING: 'existing'
+}).create().freeze();
+
+const component = CpPanelBodyComponent.extend(PreprintFormFieldMixin, {
+    state: State.START,
     didRender() {
         this.set('valid', true);
+    },
+    actions: {
+        changeState(newState) {
+            this.set('state', newState);
+        }
     }
 });
+
+component.reopen({
+    _State: State
+});
+
+export default component;
