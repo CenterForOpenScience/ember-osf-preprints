3da08e880d5797666538cc8313769dfdc52a1a81
Ellis Tsung <git@tsung.co>
2016-08-01 13:31:11 -0400

    Sort subjects

--- app/components/preprint-form-subjects.js
+++ app/components/preprint-form-subjects.js
@@ -13,17 +13,72 @@ export default CpPanelBodyComponent.extend(PreprintFormFieldMixin, {
         },
         'l': {}
     },
+    sortedTaxonomies: Ember.computed('taxonomies', function() {
+        return [{
+            name: 'a',
+            children: [{
+                name: 'b',
+                children: ['c', 'd', 'e']
+            }, {
+                name: 'f',
+                children: ['g']
+            }],
+        }, {
+            name: 'h',
+            children: [{
+                name: 'i',
+                children: ['j', 'k']
+            }]
+        }, {
+            name: 'l'
+        }];
+    }),
     taxonomy: null,
     path: [],
     selected: new Ember.Object(),
+    // sortedSelection: Ember.computed('selected', function() {
+    //     const selected = this.get('selected');
+    //     const taxonomies = Object.keys(selected);
+    //     const temp = taxonomies
+    //         .map(taxonomy => selected.get(taxonomy))
+    //         .filter(categories => categories);
+    //     const categories = temp
+    //         .reduce((prev, cur) => prev.concat(Object.keys(cur)), []);
+    //     const subjects = temp
+    //         .map(categories => Object.keys(categories)
+    //             .map(category => categories.get(category))
+    //             .filter(subjects => subjects)
+    //             .reduce((prev, cur) => prev.concat(Object.keys(cur)), []))
+    //         .reduce((prev, cur) => prev.concat(cur));
+    //     return {
+    //         taxonomies: taxonomies,
+    //         categories: categories,
+    //         subjects: subjects
+    //     };
+    // }),
+    sortedSelection: Ember.computed('selected', function() {
+        const sorted = [];
+        const selected = this.get('selected');
+        const flatten = ([obj, name = []]) => {
+            const keys = Object.keys(obj);
+            if (keys.length === 0) {
+                return name.length !== 0 && sorted.pushObject(name);
+            } else {
+                return keys.sort().map(key => [obj.get(key), [...name, key]]).forEach(flatten);
+            }
+        };
+        flatten([selected]);
+        return sorted;
+    }),
     valid: Ember.computed.oneWay('taxonomy'),
     actions: {
         delete(key) {
             this.set(key, null);
             eval(`delete this.${key}`);
         },
-        deselect(...args) {
-            this.send('delete', `selected.${args.slice(0, args.length - 1).join('.')}`);
+        deselect([...args]) {
+            this.send('delete', `selected.${args.filter(arg => Ember.typeOf(arg) === 'string').join('.')}`);
+            this.notifyPropertyChange('selected');
             this.rerender();
         },
         select(...args) {
@@ -38,8 +93,9 @@ export default CpPanelBodyComponent.extend(PreprintFormFieldMixin, {
                 return `${prev}.${cur}`;
             };
             // Process past length of array
-            process(args.reduce(process), '', args.length);
+            process(args.map(arg => arg.name || arg).reduce(process), '', args.length);
             this.set('path', args);
+            this.notifyPropertyChange('selected');
             this.rerender();
         }
     }
