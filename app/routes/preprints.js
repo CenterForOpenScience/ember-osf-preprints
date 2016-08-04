262751039be32e9f99d17ddb7794dbe49212fd44
Lauren Revere <geeksnglitter@gmail.com>
2016-08-04 11:18:44 -0400

    Fix general layout of view page

--- app/routes/preprints.js
+++ app/routes/preprints.js
@@ -7,12 +7,12 @@ export default Ember.Route.extend({
             id: params.file_id,
             baseUrl: config.OSF.url,
             renderUrl: config.OSF.renderUrl,
-            // JamDB
+
             preprint: this.store.findRecord('preprint', params.file_id),
-            project: this.store.findRecord('preprint', params.file_id).then(preprint =>
-                this.store.findRecord('file', preprint.get('path')).then(file => file.get('links').download.split('/')[5])),
-            downloadLink: this.store.findRecord('preprint', params.file_id).then(preprint =>
-                this.store.findRecord('file', preprint.get('path')).then(file => file.get('links').download))
+//            project: this.store.findRecord('preprint', params.file_id).then(preprint =>
+//                this.store.findRecord('file', preprint.get('path')).then(file => file.get('links').download.split('/')[5])),
+//            downloadLink: this.store.findRecord('preprint', params.file_id).then(preprint =>
+//                this.store.findRecord('file', preprint.get('path')).then(file => file.get('links').download))
         });
     }
 });
