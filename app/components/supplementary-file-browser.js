e4ff34de55eb526b7254ea83fcc49114c30aecb7
Lauren Revere <geeksnglitter@gmail.com>
2016-08-05 10:26:38 -0400

    Add update MFR value based on click

--- app/components/supplementary-file-browser.js
+++ app/components/supplementary-file-browser.js
@@ -3,19 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     startValue: 0,
     numShowing: 6, //Pull from automatic thing
-    fileList: Ember.computed(function(){return this.get('files')}),
-    supplementList: Ember.computed('fileList', 'fileList.[]', 'startValue', 'numShowing', function(){
-        return this.get('fileList').slice(this.get('startValue'), this.get('startValue') + this.get('numShowing'));
+//    selectedFile: primaryFile,
+    selectedFile: {name: 'hello.txt', links:{download: "https://test-files.osf.io/v1/resources/qudgr/providers/osfstorage/57a379cbda3e24004afba2d5"}},
+    supplementList: Ember.computed('files', 'files.[]', 'startValue', 'numShowing', function(){
+        return this.get('files').slice(this.get('startValue'), this.get('startValue') + this.get('numShowing'));
     }),
     actions: {
         moveLeft() {
             const start = this.get('startValue');
             const numShowing = this.get('numShowing');
-            const fileListLength = this.get('fileList').length;
-            if (start + numShowing <= fileListLength)
+            const fileListLength = this.get('files').length;
+            if (start + numShowing <= fileListLength){
                 this.set('startValue', start+numShowing);
-            else
+                }
+            else{
                 this.set('startValue', 0);
-                },
+                }
+            },
+        changeFile(file) {
+            this.set('selectedFile', file);
+            },
         },
 });
