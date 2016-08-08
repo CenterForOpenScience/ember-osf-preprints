378f68b574d1c142c258d052dfd073c668d4985c
Lauren Revere <geeksnglitter@gmail.com>
2016-08-08 12:50:13 -0400

    Fix formatting of buttons and add right animation

--- app/components/supplementary-file-browser.js
+++ app/components/supplementary-file-browser.js
@@ -1,25 +1,60 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
+    elementId: 'preprint-file-view',
     startValue: 0,
-    numShowing: 6, //Pull from automatic thing
-//    selectedFile: primaryFile,
-    selectedFile: {name: 'hello.txt', links:{download: "https://test-files.osf.io/v1/resources/qudgr/providers/osfstorage/57a379cbda3e24004afba2d5"}},
-    supplementList: Ember.computed('files', 'files.[]', 'startValue', 'numShowing', function(){
-        return this.get('files').slice(this.get('startValue'), this.get('startValue') + this.get('numShowing'));
+    scrollAnim: '',
+    numShowing: 6,
+    selectedFile: {},
+
+    fullList: Ember.computed('files', 'files.[]', 'primaryFile', function(){
+        //Returns the list with primaryFile moved to the front
+        const files = this.get('files');
+        if(files){
+            const primaryFile = this.get('primaryFile');
+            files.splice(files.indexOf(primaryFile, 1));
+            files.unshift(primaryFile);
+        }
+        return files;
     }),
+    supplementList: Ember.computed('fullList', 'fullList.[]', 'startValue', 'numShowing', function(){
+        if(this.get('fullList')){
+            return this.get('fullList').slice(this.get('startValue'), this.get('startValue') + this.get('numShowing'));
+        }
+    }),
+    init: function(){
+        this._super(...arguments);
+        this.set('selectedFile', this.get('primaryFile'));
+    },
+    didInsertElement: function () {
+        $('[data-toggle="tooltip"]').tooltip({});
+    },
     actions: {
         moveLeft() {
             const start = this.get('startValue');
             const numShowing = this.get('numShowing');
             const fileListLength = this.get('files').length;
+            this.set('scrollAnim','toRight');
+            if (start - numShowing >= 0){
+                this.set('startValue', start-numShowing);
+                }
+            else{
+                this.set('startValue', fileListLength - (fileListLength%numShowing));
+                }
+            },
+        moveRight() {
+            const start = this.get('startValue');
+            const numShowing = this.get('numShowing');
+            const fileListLength = this.get('files').length;
+            this.set('scrollAnim', 'toLeft');
             if (start + numShowing <= fileListLength){
                 this.set('startValue', start+numShowing);
                 }
             else{
                 this.set('startValue', 0);
                 }
-            },
+        },
         changeFile(file) {
             this.set('selectedFile', file);
             },
