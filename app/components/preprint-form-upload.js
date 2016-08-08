69f5c33d07599b16829807ee7e3f8e241be0864a
Ellis Tsung <git@tsung.co>
2016-08-08 11:19:10 -0400

    Add project selection

--- app/components/preprint-form-upload.js
+++ app/components/preprint-form-upload.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import CpPanelBodyComponent from 'ember-collapsible-panel/components/cp-panel-body';
 import PreprintFormFieldMixin from '../mixins/preprint-form-field';
+import loadAll from 'ember-osf/utils/load-relationship';
 
 // Enum of available states
 export const State = Ember.Object.extend(Ember.Freezable, {
@@ -10,8 +11,31 @@ export const State = Ember.Object.extend(Ember.Freezable, {
 }).create().freeze();
 
 const component = CpPanelBodyComponent.extend(PreprintFormFieldMixin, {
+    session: Ember.inject.service(),
+    currentUser: Ember.inject.service(),
+    fileManager: Ember.inject.service(),
     state: State.START,
     uploadState: State.START,
+    selectedNode: null,
+    user: null,
+    userNodes: Ember.A([]),
+    init() {
+        this._super(...arguments);
+        if (this.get('session.isAuthenticated')) {
+            this._setCurrentUser();
+        }
+    },
+    _setCurrentUser() {
+        this.get('currentUser').load().then(user => this.set('user', user));
+    },
+    onGetCurrentUser: Ember.observer('user', function() {
+        const user = this.get('user');
+        if (user) {
+            loadAll(user, 'nodes', this.get('userNodes'));
+        } else {
+            this.set('userNodes', Ember.A());
+        }
+    }),
     valid: Ember.computed('state', function() {
         const state = this.get('state');
         return [State.NEW, State.EXISTING].find(valid => state === valid);
@@ -22,6 +46,21 @@ const component = CpPanelBodyComponent.extend(PreprintFormFieldMixin, {
         },
         changeUploadState(newState) {
             this.set('uploadState', newState);
+        },
+        deselectNode() {
+            this.set('selectedNode', null);
+        },
+        selectNodeFile(file) {
+            this.send('selectFile', file.get('links.download'));
+        },
+        selectNode(node) {
+            console.log(node);
+        },
+        selectFile(url) {
+            console.log(url);
+        },
+        preUpload() {
+            return new Ember.RSVP.Promise((resolve) => resolve());
         }
     }
 });
