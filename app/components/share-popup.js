1eccaf594cc77ca8709492c7adeb11577497bbef
Lauren Revere <geeksnglitter@gmail.com>
2016-08-09 16:06:25 -0400

    Fix js style check errors

--- app/components/share-popup.js
+++ app/components/share-popup.js
@@ -3,4 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     didInsertElement: function () {
         this.$('#share-popover').popover();
-    },});
+    },
+});
--- app/components/supplementary-file-browser.js
+++ app/components/supplementary-file-browser.js
@@ -7,22 +7,22 @@ export default Ember.Component.extend({
     numShowing: 6,
     selectedFile: {},
 
-    fullList: Ember.computed('files', 'files.[]', 'primaryFile', function(){
+    fullList: Ember.computed('files', 'files.[]', 'primaryFile', function() {
         //Returns the list with primaryFile moved to the front
         const files = this.get('files');
-        if(files){
+        if (files) {
             const primaryFile = this.get('primaryFile');
             files.splice(files.indexOf(primaryFile, 1));
             files.unshift(primaryFile);
         }
         return files;
     }),
-    supplementList: Ember.computed('fullList', 'fullList.[]', 'startValue', 'numShowing', function(){
-        if(this.get('fullList')){
+    supplementList: Ember.computed('fullList', 'fullList.[]', 'startValue', 'numShowing', function() {
+        if (this.get('fullList')) {
             return this.get('fullList').slice(this.get('startValue'), this.get('startValue') + this.get('numShowing'));
         }
     }),
-    init: function(){
+    init: function() {
         this._super(...arguments);
         this.set('selectedFile', this.get('primaryFile'));
     },
@@ -31,28 +31,26 @@ export default Ember.Component.extend({
             const start = this.get('startValue');
             const numShowing = this.get('numShowing');
             const fileListLength = this.get('files').length;
-            this.set('scrollAnim','toRight');
-            if (start - numShowing >= 0){
-                this.set('startValue', start-numShowing);
-                }
-            else{
-                this.set('startValue', fileListLength - (fileListLength%numShowing));
-                }
-            },
+            this.set('scrollAnim', 'toRight');
+            if (start - numShowing >= 0) {
+                this.set('startValue', start - numShowing);
+            }else {
+                this.set('startValue', fileListLength - (fileListLength % numShowing));
+            }
+        },
         moveRight() {
             const start = this.get('startValue');
             const numShowing = this.get('numShowing');
             const fileListLength = this.get('files').length;
             this.set('scrollAnim', 'toLeft');
-            if (start + numShowing <= fileListLength){
-                this.set('startValue', start+numShowing);
-                }
-            else{
+            if (start + numShowing <= fileListLength) {
+                this.set('startValue', start + numShowing);
+            }else {
                 this.set('startValue', 0);
-                }
+            }
         },
         changeFile(file) {
             this.set('selectedFile', file);
-            },
         },
+    },
 });
