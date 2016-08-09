b13978dd64e0dcabbf705b6863839d9170096caa
Andy Boughton <abought@gmail.com>
2016-08-09 09:37:05 -0400

    Add taxonomy models.
    This breaks ember preprints at this instant in time, insofar as the endpoint should not be defined as a list view. We're keeping the ember code correct (expect single record for future planned bugfix), so watch for temporary model hook failures.
    
    [ci skip]
    [#PREP-97]

--- app/routes/browse.js
+++ app/routes/browse.js
@@ -12,7 +12,7 @@ export default Ember.Route.extend({
     model() {
         return Ember.RSVP.hash({
             preprints: this.store.findAll('preprint'),
-            taxonomy: this.store.find('taxonomy', 1)
+            taxonomy: this.store.find('taxonomy', 'plos')
         });
     },
     actions: {
