cf6c73c0c5ebd2b2fabf5850d65890cef7a1c796
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-09 20:09:28 -0400

    Add user-validation component for validation unregistered contributors form.

---
+++ app/components/user-validation.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+
+import {
+  validator, buildValidations
+}
+from 'ember-cp-validations';
+
+/**
+* Validations for adding unregistered contributor form.  fullName must be present
+* and have three letters, and the username (email) must be present and of appropriate format.
+*/
+const Validations = buildValidations({
+    fullName: {
+        description: 'Full Name',
+        validators: [
+            validator('presence', true),
+            validator('length', {
+                min: 3
+            })
+        ]
+    },
+    username: {
+        validators: [
+            validator('presence', true),
+            validator('format', {
+                type: 'email'
+            })
+       ]
+    },
+});
+
+export default Ember.Component.extend(Validations, {
+    fullName: null,
+    username: null,
+    isFormValid: Ember.computed.alias('validations.isValid'),
+    actions: {
+        searchView() {
+            this.sendAction('searchView');
+        },
+        addUnregisteredContributor(fullName, email) {
+            this.sendAction('addUnregisteredContributor', fullName, email);
+
+        }
+    }
+});
