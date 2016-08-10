14f5ab15e251eca6f1a03a54021e995dbcc89bf5
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-09 20:04:16 -0400

    Add validated-input component modified from ember-cp-validations dummy app.

---
+++ app/components/validated-input.js
@@ -0,0 +1,44 @@
+// Copied from ember-cpi-validations/tests/dummy/app - should eventually be part of ember-osf but getting errors with dependencies
+
+// BEGIN-SNIPPET validated-input
+import Ember from 'ember';
+
+const {
+  isEmpty,
+  computed,
+  defineProperty,
+} = Ember;
+
+export default Ember.Component.extend({
+    classNames: ['validated-input'],
+    classNameBindings: ['showErrorClass:has-error', 'isValid:has-success'],
+    model: null,
+    value: null,
+    type: 'text',
+    valuePath: '',
+    placeholder: '',
+    validation: null,
+    isTyping: false,
+
+    init() {
+        this._super(...arguments);
+        var valuePath = this.get('valuePath');
+        defineProperty(this, 'validation', computed.oneWay(`model.validations.attrs.${valuePath}`));
+        defineProperty(this, 'value', computed.alias(`model.${valuePath}`));
+    },
+
+    notValidating: computed.not('validation.isValidating'),
+    didValidate: computed.oneWay('targetObject.didValidate'),
+    hasContent: computed.notEmpty('value'),
+    isValid: computed.and('hasContent', 'validation.isValid', 'notValidating'),
+    isInvalid: computed.oneWay('validation.isInvalid'),
+    showErrorClass: computed.and('notValidating', 'showMessage', 'hasContent', 'validation'),
+    showErrorMessage: computed('validation.isDirty', 'isInvalid', 'didValidate', function() {
+        return (this.get('validation.isDirty') || this.get('didValidate')) && this.get('isInvalid');
+    }),
+
+    showWarningMessage: computed('validation.isDirty', 'validation.warnings.[]', 'isValid', 'didValidate', function() {
+        return (this.get('validation.isDirty') || this.get('didValidate')) && this.get('isValid') && !isEmpty(this.get('validation.warnings'));
+    })
+});
+// END-SNIPPET
---
+++ tests/integration/components/validated-input-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('validated-input', 'Integration | Component | validated input', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{validated-input}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#validated-input}}
+      template block text
+    {{/validated-input}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
