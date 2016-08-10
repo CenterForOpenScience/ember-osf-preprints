ce724e45aee44603e6983a7e0411888498c53932
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-09 20:09:59 -0400

    Add contributor related actions to preprint-form.

--- app/components/preprint-form.js
+++ app/components/preprint-form.js
@@ -10,6 +10,38 @@ export default CpPanelsComponent.extend({
     actions: {
         verify(name, state) {
             this.get('valid').set(name, state);
+        },
+        addContributor(userId) {
+            this.sendAction('addContributor', userId, 'write', true);
+        },
+        addUnregisteredContributor(fullName, email) {
+            let res = this.attrs.addUnregisteredContributor(fullName, email, 'write', true);
+        },
+        findContributors(query, page) {
+            this.sendAction('findContributors', query, page);
+
+        },
+        removeContributor(contrib) {
+            this.sendAction('removeContributor', contrib);
+        },
+        updatePermissions(contributor, permission) {
+            this.sendAction(
+                'editContributors',
+                this.get('contributors'),
+                this.get('permissionChanges'),
+                {}
+            );
+        },
+        updateBibliographic(contributor, isBibliographic) {
+            this.sendAction(
+                'editContributors',
+                this.get('contributors'),
+                {},
+                this.get('bibliographicChanges')
+            );
+        },
+        editContributors(contributors, permissions, bibliographic) {
+            this.sendAction('editContributors', contributors, permissions, bibliographic);
         }
     }
 });
