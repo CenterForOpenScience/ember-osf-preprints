1d54aa2502eed1e0d0901b981f85b0b32b6dc062
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-11 10:37:20 -0400

    Use permissionSelector constants in permissionMap helper.

--- app/helpers/permission-map.js
+++ app/helpers/permission-map.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import permissions, { permissionSelector } from 'ember-osf/const/permissions';
 
 /**
 * permissionMap helper.  Maps short form of the contributor's permissions
@@ -6,11 +7,10 @@ import Ember from 'ember';
 *
 */
 export function permissionMap(params/*, hash*/) {
-    var map = {
-        read: 'Read',
-        write: 'Read + Write',
-        admin: 'Administrator'
-    };
+    var map = {};
+    for (var i = 0; i < permissionSelector.length; i++) {
+        map[permissionSelector[i].value] = permissionSelector[i].text;
+    }
     var permission = params[0];
     return map[permission];
 }
