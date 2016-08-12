98cf5d072a0f60790cae185da227b4792aab899a
Henrique Moco <hmoco94@vt.edu>
2016-08-12 13:50:33 -0400

    add filtering functionality

---
+++ app/helpers/if-filter.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export function ifFilter(params) {
+    if ((typeof params[1] === 'undefined') || ((params[0].toLowerCase()).indexOf(params[1].toLowerCase()) !== -1)){
+        return true;
+    }
+    return false;
+}
+
+export default Ember.Helper.helper(ifFilter);
---
+++ tests/unit/helpers/if-filter-test.js
@@ -0,0 +1,10 @@
+import { ifFilter } from 'preprint-service/helpers/if-filter';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | if filter');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = ifFilter([42]);
+  assert.ok(result);
+});
