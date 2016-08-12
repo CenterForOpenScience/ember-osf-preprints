1a7c285fc0f18e23fd6a35e3f503bb7f7faf974f
Henrique Moco <hmoco94@vt.edu>
2016-08-12 10:22:14 -0400

    working subject selection

--- app/controllers/add-preprint.js
+++ app/controllers/add-preprint.js
@@ -42,57 +42,36 @@ export default Ember.Controller.extend({
             this.set('userNodes', Ember.A());
         }
     }),
-
+    changeFlag: 'changed',
     filter: [{}, {}, {}],
-    filteredPath: Ember.computed('path', 'filter', 'filter.@each.value', function() {
-        console.log('PPASDoAODAOODskdOKdsoakoASDOAKSODkOAKSOdkoako');
+    updateFilteredPath() {
         var _this = this;
+        var overallPath = []
         this.get('path').slice(0, 2).map((path, i) => {
             if (!path.children) {
                 _this.store.query('taxonomy', { filter: { parent_ids: path.id}, page: {size: 100} }).then(
                     results => {path.children = results.map(
                         result => {return {name: result.get('text'), id: result.id}}
                     )
-                    _this.set('filteredPath', [path]);
+                    overallPath.push(path);
+                    _this.set('filteredPath', overallPath);
                     }
                 );
+            } else {
+                overallPath.push(path);
             }
         });
-        // return this.get('path').slice(0, 2).map((path, i) => {
-        //     if (path.children && this.get(`filter.${i + 1}.value`)) {
-        //         return {
-        //             name: path.name,
-        //             children: path.children.filter(child =>
-        //                 this.get(`filter.${i + 1}.value`).indexOf(child.name || child)) !== -1
-        //         };
-        //     }
-        //     return path;
-        // });
+    },
+    filteredPath: Ember.computed('path', 'changeFlag', 'filter', 'filter.@each.value', function() {
+        this.updateFilteredPath();
     }),
     sortedTaxonomies: Ember.computed('taxonomies', 'filter', 'filter.0.value', function() {
-        // return [{
-        //     name: 'a',
-        //     children: [{
-        //         name: 'b',
-        //         children: ['c', 'd', 'e']
-        //     }, {
-        //         name: 'f',
-        //         children: ['g']
-        //     }],
-        // }, {
-        //     name: 'h',
-        //     children: [{
-        //         name: 'i',
-        //         children: ['j', 'k']
-        //     }]
-        // }, {
-        //     name: 'l'
-        // }].filter(taxonomy =>
-        //     !this.get('filter.0.value') || taxonomy.name.indexOf(this.get('filter.0.value')) !== -1
-        // );
+
         var self = this;
         this.store.query('taxonomy', { filter: { parent_ids: 'null'}, page: {size: 100} }).then(results => {
-            self.set('sortedTaxonomies', results.map( result => {return {name: result.get('text'), id: result.get('id')}}));
+            self.set('sortedTaxonomies', results.map( result => {
+                return {name: result.get('text'), id: result.get('id')};
+            }));
         });
     }),
     path: [],
@@ -172,21 +151,8 @@ export default Ember.Controller.extend({
             // Process past length of array
             [...args.map(arg => arg.name || arg), ''].reduce(process);
             this.set('path', args);
+            this.updateFilteredPath();
             this.notifyPropertyChange('selected');
-            this.notifyPropertyChange('filter');
-            var _this = this;
-            this.get('path').slice(0, 2).map((path, i) => {
-                if (!path.children) {
-                    _this.store.query('taxonomy', { filter: { parent_ids: path.id}, page: {size: 100} }).then(
-                        results => {path.children = results.map(
-                            result => {return {name: result.get('text'), id: result.id}}
-                        )
-                        _this.set('filteredPath', [path]);
-                        }
-                    );
-                }
-            });
-            //this.rerender();
         }
     }
 });
