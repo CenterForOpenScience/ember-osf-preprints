6bde9f2fe6923f9d1af0a177c6da8acfba8d7077
Henrique Moco <hmoco94@vt.edu>
2016-08-15 10:18:09 -0400

    docs and lint

--- app/helpers/if-filter.js
+++ app/helpers/if-filter.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
+/**
+ * To be used within an {#if} block, to only displays an item if it matches a filter.
+ *
+ * @class ifFilter
+ * @param params: 0 indicates the element, 1 indicates the filter
+ */
 export function ifFilter(params) {
     if ((typeof params[1] === 'undefined') || ((params[0].toLowerCase()).indexOf(params[1].toLowerCase()) !== -1)) {
         return true;
