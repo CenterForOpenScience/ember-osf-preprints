5a14696a7c41f2dca317c5f66a14ac7dde237487
Henrique Moco <hmoco94@vt.edu>
2016-08-16 09:09:44 -0400

    fix last test

--- tests/integration/components/taxonomy-tree-test.js
+++ tests/integration/components/taxonomy-tree-test.js
@@ -8,6 +8,7 @@ moduleForComponent('taxonomy-tree', 'Integration | Component | taxonomy tree', {
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`{{taxonomy-tree subject=''}}`);
 
-  assert.ok(this.render(hbs`{{taxonomy-tree subject=''}}`));
+  assert.equal(this.$().text().trim(), '');
 });
