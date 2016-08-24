757e51c1ee0bd284a29d77033317db62f46fcab9
erinspace <erin.braswell@gmail.com>
2016-08-24 10:52:05 -0400

    Add ajax call to get total preprints from SHARE

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,26 @@ import Ember from 'ember';
 import config from 'ember-get-config';
 
 export default Ember.Route.extend({
+
     fileManager: Ember.inject.service(),
     model() {
+        var getTotalPayload = '{"size": 0, "from": 0,"query": {"bool": {"must": {"query_string": {"query": "*"}}, "filter": [{"term": {"type.raw": "preprint"}}]}}}';
+
+        var sharePreprintsTotal = Ember.$.ajax({
+                type: 'POST',
+                url: config.SHARE.searchUrl,
+                data: getTotalPayload,
+                contentType: 'application/json',
+                crossDomain: true,
+            }).then(function (results) {
+                return results.hits.total;
+            });
+
         return Ember.RSVP.hash({
             theDate: new Date(),
             subjects: this.store.query('taxonomy', { filter: { parents: 'null' }, page: { size: 20 } }),
             preprints: this.store.findRecord('preprint-provider', config.PREPRINTS.provider).then(provider => provider.get('preprints')),
+            sharePreprintsTotal: sharePreprintsTotal
         });
     },
     actions: {
