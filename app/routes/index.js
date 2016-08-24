bebf14ce7c1c6847758bf8bcc3a227680e9d74b8
erinspace <erin.braswell@gmail.com>
2016-08-24 16:06:02 -0400

    Way more useful way to do this via @abought

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,9 +7,6 @@ export default Ember.Route.extend({
     model() {
         var getTotalPayload = '{"size": 0, "from": 0,"query": {"bool": {"must": {"query_string": {"query": "*"}}, "filter": [{"term": {"type.raw": "preprint"}}]}}}';
 
-        function formatNumberWithCommas(x) {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
         var sharePreprintsTotal = Ember.$.ajax({
                 type: 'POST',
                 url: config.SHARE.searchUrl,
@@ -17,7 +14,7 @@ export default Ember.Route.extend({
                 contentType: 'application/json',
                 crossDomain: true,
             }).then(function (results) {
-                return formatNumberWithCommas(results.hits.total);
+                return results.hits.total.toLocaleString();
             });
 
         return Ember.RSVP.hash({
