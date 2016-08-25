04cb43d86837c663fb9aed768e3f6ed3ba3eec86
Chris Seto <chriskseto@gmail.com>
2016-08-25 17:26:20 -0400

    Clean up supplementary files browser

--- app/routes/content.js
+++ app/routes/content.js
@@ -3,23 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(params) {
         return this.store.findRecord('preprint', params.preprint_id);
-    },
-    setupController(controller, model) {
-        this.getFiles(model).then(files => controller.set('fileList', files));
-        model.query('contributors', { 'page[size]': 100 }).then(authors => controller.set('authors', authors));
-        this._super(...arguments);
-    },
-    getFiles(node) {
-        //TODO: Make supplement scrolling based on pagination
-        return node.query(
-        'files', { 'filter[name]': 'osfstorage' }
-        ).then(
-            providers => {
-                var provider = providers.get('firstObject');
-                if (provider) {
-                    return provider.query('files', { 'page[size]': 100 });
-                }
-            }
-        );
     }
 });
