9a6ba2cea25516463cedd1462cc511c6fb18ac53
Andy Boughton <abought@gmail.com>
2016-08-27 16:47:04 -0400

    Appease linters

--- app/mixins/reset-scroll.js
+++ app/mixins/reset-scroll.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  activate: function() {
-      this._super();
-      window.scrollTo(0,0);
-  }
+    activate: function() {
+        this._super();
+        window.scrollTo(0, 0);
+    }
 });
