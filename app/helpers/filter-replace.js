fd4fc768b3faaf6b6c4ba2c7e3012a90c36b1654
Henrique Moco <hmoco94@vt.edu>
2016-08-27 17:14:24 -0400

    Add filter-replce to replace long provider names without messing with search filter logic

---
+++ app/helpers/filter-replace.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+var filters = {
+    'Open Science Framework': 'OSF',
+    'Cognitive Sciences ePrint Archive': 'Cogprints'
+};
+
+export function filterReplace(params) {
+    return filters[params[0]] ? filters[params[0]] : params[0];
+};
+
+export default Ember.Helper.helper(filterReplace);
