c846e0f7d27553554da42bca6906ddce7034e7b6
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-27 14:27:48 -0400

    After creating preprint, scroll to top of content page.

--- app/routes/content.js
+++ app/routes/content.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import resetScrollMixin from '../mixins/reset-scroll';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(resetScrollMixin, {
     model(params) {
         return this.store.findRecord('preprint', params.preprint_id);
     },
