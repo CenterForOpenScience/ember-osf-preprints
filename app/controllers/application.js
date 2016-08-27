ff31a0624241a8489b3b401e5be3134434bc51fa
icereval <icereval@gmail.com>
2016-08-27 11:33:39 -0400

    redirect signin through flask login endpoint

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import OSFAgnosticAuthControllerMixin from 'ember-osf/mixins/osf-agnostic-auth-controller';
+import { getAuthUrl } from 'ember-osf/utils/auth';
 
 import config from '../config/environment';
 
@@ -7,4 +8,11 @@ export default Ember.Controller.extend(OSFAgnosticAuthControllerMixin, {
     toast: Ember.inject.service(),
 
     signupUrl: `${config.OSF.url}login?` + Ember.$.param({campaign: config.PREPRINTS.campaign}),
+
+    actions: {
+        login() {
+            // TODO: needs to work w/ token auth, for now redirect login through flask endpoint and include next url
+            window.location = getAuthUrl(`${config.OSF.url}login?next=${encodeURI(window.location)}`);
+        }
+    }
 });
