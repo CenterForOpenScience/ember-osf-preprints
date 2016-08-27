ef235d85dec0eee4ca7dfa936618f1b53b02649d
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-27 18:00:08 -0400

    Remove loading all user nodes.

--- app/routes/submit.js
+++ app/routes/submit.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 import CasAuthenticatedRouteMixin from 'ember-osf/mixins/cas-authenticated-route';
-import permissions from 'ember-osf/const/permissions';
-import loadAll from 'ember-osf/utils/load-relationship';
 import ResetScrollMixin from '../mixins/reset-scroll';
 
 export default Ember.Route.extend(ResetScrollMixin, CasAuthenticatedRouteMixin, {
@@ -22,20 +20,12 @@ export default Ember.Route.extend(ResetScrollMixin, CasAuthenticatedRouteMixin, 
     },
     setupController(controller) {
         // Fetch values required to operate the page: user and userNodes
-        let userNodes = Ember.A();
-
         this.get('currentUser').load()
             .then((user) => {
                 controller.set('user', user);
                 return user;
-            }).then((user) => loadAll(user, 'nodes', userNodes, {
-                'filter[preprint]': false
-            }).then(() => {
-                // TODO Hack: API does not support filtering current_user_permissions in the way we desire, so filter
-                // on front end for now until filtering support can be added to backend
-                let onlyAdminNodes = userNodes.filter((item) => item.get('currentUserPermissions').indexOf(permissions.ADMIN) !== -1);
-                controller.set('userNodes', onlyAdminNodes);
-            }));
+            }
+       );
         return this._super(...arguments);
     },
     actions: {
