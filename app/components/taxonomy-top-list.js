a3805dceb64652859d0ebee858596efa468dcee2
Henrique Moco <hmoco94@vt.edu>
2016-08-29 14:19:45 -0400

    Add extra tooltips, use hint css

--- app/components/taxonomy-top-list.js
+++ app/components/taxonomy-top-list.js
@@ -2,62 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     subjectTooltips: {
-        'Physical sciences': '<p>Astronomical Sciences</p>' +
-            '<p>Chemistry</p>' +
-            '<p>Materials Science</p>' +
-            '<p>Mathematics</p>' +
-            '<p>Physics</p>',
-        'Engineering and technology': '<p>Civil Engineering</p>' +
-            '<p>Digital Imaging</p>' +
-            '<p>Fluidics</p>' +
-            '<p>Nanotechnology</p>' +
-            '<p>Synthetic Biology</p>',
-        'Biology and life sciences': '<p>Ecology</p>' +
-            '<p>Genetics</p>' +
-            '<p>Neuroscience</p>' +
-            '<p>Toxicology</p>' +
-            '<p>Zoology</p>',
-        'Research and analysis methods': '<p>Computational Techniques</p>' +
-            '<p>Decision Analysis</p>' +
-            '<p>Imaging Techniques</p>' +
-            '<p>Research Assessment</p>' +
-            '<p>Simulation and Modeling</p>',
-        'Medicine and health sciences': '<p>Anatomy</p>' +
-            '<p>Epidemiology</p>' +
-            '<p>Medical Ethics</p>' +
-            '<p>Sports and Exercise Medicine</p>' +
-            '<p>Oncology</p>',
-        'Social sciences': '<p>Anthropology</p>' +
-            '<p>Economics</p>' +
-            '<p>Philosophy</p>' +
-            '<p>Political Science</p>' +
-            '<p>Psychology</p>',
-        'Science policy': '<p>Bioethics</p>' +
-            '<p>Open Science</p>' +
-            '<p>Research Integrity</p>' +
-            '<p>Science Education</p>' +
-            '<p>Technology Regulations</p>',
-        'People and places': '<p>Demography</p>' +
-            '<p>Geographical Locations</p>' +
-            '<p>Population groups</p>',
-        'Computer and information sciences': '<p>Artificial Intelligence</p>' +
-            '<p>Cryptography</p>' +
-            '<p>Data Visualization</p>' +
-            '<p>Library Science</p>' +
-            '<p>Software Engineering</p>',
-        'Earth sciences': '<p>Atmospheric Science</p>' +
-            '<p>Geology</p>' +
-            '<p>Hydrology</p>' +
-            '<p>Marine and aquatic sciences</p>' +
-            '<p>Mineralogy</p>',
-        'Ecology and environmental sciences': '<p>Biogeochemistry</p>' +
-            '<p>Environmental Geology</p>' +
-            '<p>Natural Resources</p>' +
-            '<p>Soil Science</p>' +
-            '<p>Sustainability Science</p>'
-    },
-    didRender() {
-        Ember.$('.subject-tooltips').tooltip();
+        'Physical sciences': 'Astronomical Sciences, Chemistry, Materials Science, Mathematics, Physics',
+        'Engineering and technology': 'Civil Engineering, Digital Imaging, Fluidics, Nanotechnology, Synthetic Biology',
+        'Biology and life sciences': 'Ecology, Genetics, Neuroscience, Toxicology, Zoology',
+        'Research and analysis methods': 'Computational Techniques, Decision Analysis, Imaging Techniques, Research Assessment, Simulation and Modeling',
+        'Medicine and health sciences': 'Anatomy, Epidemiology, Medical Ethics, Sports and Exercise Medicine, Oncology',
+        'Social sciences': 'Anthropology, Economics, Philosophy, Political Science, Psychology',
+        'Science policy': 'Bioethics, Open Science, Research Integrity, Science Education, Technology Regulations',
+        'People and places': 'Demography, Geographical Locations, Population groups',
+        'Computer and information sciences': 'Artificial Intelligence, Cryptography, Data Visualization, Library Science, Software Engineering',
+        'Earth sciences': 'Atmospheric Science, Geology, Hydrology, Marine and aquatic sciences, Mineralogy',
+        'Ecology and environmental sciences': 'Biogeochemistry, Environmental Geology, Natural Resources, Soil Science, Sustainability Science',
+        'Business': 'Accounting, Finance and Financial Management, Human Resource Management, Marketing, Taxation',
+        'Law': 'Civil Law, Criminal Law, Legislation, State and Local Government Law, Supreme Court of the United State',
+        'Education': 'Bilingual, Multilingual and Multicultural Education, Curriculum Instruction, Educational Administration and Supervision, Higher Education, Teacher Education and Professional Development',
+        'Arts and Humanities': 'Fine Arts, History, Music, Philosophy, Religion',
+        'Architecture': 'Architectural Engineering, Construction Engineering, Environmental Design, Interior Architecture, Landscape Architecture'
     },
     sortedList: Ember.computed('list', 'list.content', function() {
         if (!this.get('list')) {
