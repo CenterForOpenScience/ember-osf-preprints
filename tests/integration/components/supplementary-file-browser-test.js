ad452ef028543eab91cec7a275c793ba7c6aa330
Andy Boughton <abought@gmail.com>
2016-08-29 15:11:33 -0400

    Mock out requirement for file browser test
    [ci skip]

--- tests/integration/components/supplementary-file-browser-test.js
+++ tests/integration/components/supplementary-file-browser-test.js
@@ -3,14 +3,31 @@ import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 
 moduleForComponent('supplementary-file-browser', 'Integration | Component | supplementary file browser', {
-  integration: true
+    integration: true
 });
 
 test('it renders', function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
 
-    this.set('preprint', Ember.RSVP.resolve());
+    // Construct a fake preprint object to satisfy browser needs
+    let providerFiles = () => Ember.RSVP.resolve(Ember.ArrayProxy.create({
+        content: Ember.A(),
+        meta: {
+            pagination: {
+                total: 1
+            }
+        }}));
+    let providersQuery =  Ember.RSVP.resolve(Ember.A([{
+            name: 'osfstorage',
+            query: providerFiles
+    }]));
+    let preprint = Ember.Object.create({
+        files: providersQuery
+    });
+
+    this.set('preprint', preprint);
+
     this.render(hbs`{{supplementary-file-browser preprint=preprint}}`);
 
     assert.equal(this.$().text().trim(), '');
