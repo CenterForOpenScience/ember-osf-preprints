5efd368c1096692734524bd85a1c99621915e3c8
Henrique Moco <hmoco94@vt.edu>
2016-08-28 23:28:07 -0400

    add logic for most likely paper link to be used first

--- app/components/search-result.js
+++ app/components/search-result.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    providerUrlRegex: {
+        //'bioRxiv': '', doesnt currently have urls
+        'Cognitive Sciences ePrint Archive': 'cogprints',
+        OSF: 'osf',
+        PeerJ: 'peerj',
+        arXiv: 'arxiv'
+    },
 
     numMaxChars: 300,
     showBody: false,
@@ -24,7 +31,12 @@ export default Ember.Component.extend({
     }.property('result'),
 
     hyperlink: function() {
-        return this.get('result.lists.links.0.url');
+        var rule = this.get('providerUrlRegex')[this.get('result.providers.0.name')];
+        var matches = [];
+        if (rule) {
+            matches = this.get('result.lists.links').slice().filter(each => each.url.indexOf(rule) !== -1);
+        }
+        return matches.length ? matches[0].url : this.get('result.lists.links.0.url');
     }.property('result'),
 
     actions: {
