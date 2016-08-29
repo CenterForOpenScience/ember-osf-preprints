7dcb22e442b61326803c6517cbc447348cb28f22
erinspace <erin.braswell@gmail.com>
2016-08-29 00:12:26 -0400

    Fix syntax in taxonomy model

--- app/models/taxonomy.js
+++ app/models/taxonomy.js
@@ -15,6 +15,6 @@ import OsfModel from 'ember-osf/models/osf-model';
 export default OsfModel.extend({
     text: DS.attr('string'),
     // TODO: Api implements this as a list field for now. This should be a relationship field in the future, when API supports it
-    child_count: DS.attr()
-    parents: DS.attr(),
+    child_count: DS.attr(),
+    parents: DS.attr()
 });
