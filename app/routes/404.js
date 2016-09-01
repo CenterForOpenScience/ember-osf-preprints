8dc305a2f44bbdf5cb26b8fef12440199382bbb6
erinspace <erin.braswell@gmail.com>
2016-09-01 16:53:13 -0400

    Switch to intermediateTransitionTo to keep the URL the same for not found links

--- app/routes/404.js
+++ app/routes/404.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     beforeModel() {
-        this.transitionTo('page-not-found');
+        this.intermediateTransitionTo('page-not-found');
     }
 });
--- app/routes/content.js
+++ app/routes/content.js
@@ -11,7 +11,7 @@ export default Ember.Route.extend(ResetScrollMixin, {
     },
     actions: {
         error: function() {
-            this.transitionTo('/page-not-found');
+            this.intermediateTransitionTo('/page-not-found');
         }
     }
 });
