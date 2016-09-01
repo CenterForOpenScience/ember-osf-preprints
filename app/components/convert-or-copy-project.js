133a40864a8a0e51f252bbb25a0c8f1f3cac71ae
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-09-01 17:48:10 -0400

    Convert-or-copy component can set node title as current node title if convert.

--- app/components/convert-or-copy-project.js
+++ app/components/convert-or-copy-project.js
@@ -4,13 +4,17 @@ import Ember from 'ember';
  * Convert Or Copy Widget - very simple, just presents decision, do you want to convert this project
  *  or copy file to a new component.
  *
- *  Will set passed in argument createComponent to true or false.
+ *  Will set convertOrCopy to 'convert' or 'copy'.  If convert, will set node title as current node title and titleValid to true.
+ *  If 'copy', nodeTitle will be set equal to null, and titleValid to false.
  *
  * Sample usage:
  * ```handlebars
  * {{convert-or-copy-project
  *  convertOrCopy=convertOrCopy
-}}
+ *  titleValid=titleValid
+ *  nodeTitle=nodeTitle
+ *  node=node
+ *}}
  * ```
  * @class convert-or-copy-project
  */
@@ -18,9 +22,15 @@ export default Ember.Component.extend({
     actions: {
         copyToComponent() {
             this.set('convertOrCopy', 'copy');
+            this.set('nodeTitle', null);
+            this.set('titleValid', false);
         },
         convertExisting() {
             this.set('convertOrCopy', 'convert');
-        },
+            if (this.get('node')) {
+                this.set('nodeTitle', this.get('node.title'));
+                this.set('titleValid', true);
+            }
+        }
     }
 });
