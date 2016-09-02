46af80c5fc9fba8a56855fa1ce019c9a37f433bc
Henrique Moco <hmoco94@vt.edu>
2016-09-02 16:45:13 -0400

    add links and actions to the share buttons

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -5,7 +5,18 @@ export default Ember.Controller.extend({
     fullScreenMFR: false,
     expandedAuthors: true,
     expandShare: false,
-
+    twitterHref: Ember.computed('model', function() {
+        return 'https://twitter.com/intent/tweet?url=' + window.location.href + '&text=' + this.get('model.title') + '&via=OSFramework';
+    }),
+    facebookHref: Ember.computed('model', function() {
+        return 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href;
+    }),
+    linkedinHref: Ember.computed('model', function() {
+        return 'https://www.linkedin.com/cws/share?url=' + window.location.href + '&title=' + this.get('model.title');
+    }),
+    emailHref: Ember.computed('model', function() {
+        return 'mailto:?subject=' + this.get('model.title') + '&body=' + window.location.href;
+    }),
     // The currently selected file (defaults to primary)
     activeFile: null,
 
@@ -35,6 +46,11 @@ export default Ember.Controller.extend({
         },
         chooseFile(fileItem) {
             this.set('activeFile', fileItem);
+        },
+        shareLink(href) {
+            window.open(href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=600,height=400');
+            this.toggleProperty('expandShare');
+            return false;
         }
     },
 });
