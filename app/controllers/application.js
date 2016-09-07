1d42863b46d5f10a0d9cf5ca676c218d75c3e4dc
Henrique Moco <hmoco94@vt.edu>
2016-09-07 14:24:13 -0400

    add provider to config

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,6 +5,21 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend(OSFAgnosticAuthControllerMixin, {
     toast: Ember.inject.service(),
+    store: Ember.inject.service(),
+
+    init() {
+        let provider_url = window.location.host;
+        //FOR TESTING
+        if (provider_url === '127.0.0.1:5000') {
+            provider_url = 'http://engrxiv.org';
+        }
+        ///
+        this.get('store').query('preprint-provider', {filter: {external_url: provider_url}}).then(function(results) {
+            if (results.get('length') === 1) {
+                results.map(each => config.APP.provider = each.id);
+            }
+        });
+    },
 
     signupUrl: `${config.OSF.url}login?` + Ember.$.param({campaign: config.PREPRINTS.campaign}),
 });
