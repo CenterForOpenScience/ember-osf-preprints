fb2bfc2b7618b771215a1718cbed82838ab43f0e
Henrique Moco <hmoco94@vt.edu>
2016-09-08 12:16:48 -0400

    add citation widget component

---
+++ app/components/citation-widget/component.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import config from 'ember-get-config';
+
+export default Ember.Component.extend({
+    apa: '',
+    mla: '',
+    chicago: '',
+    didReceiveAttrs() {
+        this._super(...arguments);
+        let url = config.apiUrl + 'nodes/' + this.get('nodeId') + '/citation/';
+        Ember.$.ajax({
+            type: 'GET',
+            url: url + 'apa/',
+            contentType: 'application/json',
+            crossDomain: true,
+        }).then(result => this.set('apa', result.citation));
+        Ember.$.ajax({
+            type: 'GET',
+            url: url + 'modern-language-association/',
+            contentType: 'application/json',
+            crossDomain: true,
+        }).then(result => this.set('mla', result.citation));
+        Ember.$.ajax({
+            type: 'GET',
+            url: url + 'chicago-author-date/',
+            contentType: 'application/json',
+            crossDomain: true,
+        }).then(result => this.set('chicago', result.citation));
+    }
+});
