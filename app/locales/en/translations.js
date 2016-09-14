c7d3d87ddcb30a2072c60d3a44ee5236723a4a81
Barrett K. Harber <barrett.harber@gmail.com>
2016-09-14 09:17:00 -0400

    Add ember-i18n Suppress lint errors for bare strings Add default locale as en to environment Add text-transform to capitalize headings Add function to set the locale to the browser's (preferred) language selection Add en locale strings

---
+++ app/locales/en/translations.js
@@ -0,0 +1,31 @@
+export default {
+    index: {
+        header: {
+            title: {
+                heading: `OSF Preprints`,
+                paragraph: `The <span class="f-w-lg">open</span> preprint repository network`
+            },
+            search: `{{count}} searchable preprints`,
+            as_of: `as of`
+        },
+        services: {
+            top: {
+                heading: `Preprint Services`,
+                paragraph: `Leading preprint service providers use this open source infrastructure to support their communities.`
+            },
+            bottom: {
+                p1: `Create your own branded preprint servers backed by the OSF.`,
+                div: {
+                    line1: `Check out the <a class="source_code_link" href="{{href}}"> open source code</a>`,
+                    line2: `and the <a class="source_code_link" href="{{href}}">requirements and road map</a>.`,
+                    line3: `Input welcome!`
+                },
+                contact: `Contact us`
+            }
+        },
+        advisory: {
+            heading: `Advisory Group`,
+            paragraph: `Our advisory group includes leaders in preprints and scholarly communication`
+        }
+    },
+};
