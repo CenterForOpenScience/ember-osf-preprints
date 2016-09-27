5a199fd8ec0cc3b107fbc9b6322674bccd4d19e4
Barrett K. Harber <barrett.harber@gmail.com>
2016-09-27 12:53:57 -0400

    Move query string generation out to its own function, update LinkedIn sharing URL

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -1,20 +1,82 @@
 import Ember from 'ember';
 import loadAll from 'ember-osf/utils/load-relationship';
 
+/**
+ * Takes an object with query parameter name as the key and [value, maxLength] as the values.
+ *
+ * @param queryParams {!object}
+ * @param queryParams.key {!array|!string}
+ * @param queryParams.key[0] {!string}
+ * @param queryParams.key[1] {int}
+ * @returns {string}
+ */
+function queryStringify(queryParams) {
+    const query = [];
+
+    for (let param in queryParams) {
+        let value = queryParams[param];
+        let maxLength = null;
+
+        if (Array.isArray(value)) {
+            value = value[0];
+            maxLength = value[1];
+        }
+
+        if (!value)
+            continue;
+
+        value = encodeURIComponent(value);
+
+        if (maxLength)
+            value = value.slice(0, maxLength);
+
+        query.push(`${param}=${value}`);
+    }
+
+    return query.join('&');
+}
+
 export default Ember.Controller.extend({
     fullScreenMFR: false,
     expandedAuthors: true,
     twitterHref: Ember.computed('model', function() {
-        return encodeURI('https://twitter.com/intent/tweet?url=' + window.location.href + '&text=' + this.get('model.title') + '&via=OSFramework');
+        const queryParams = {
+            url: window.location.href,
+            text: this.get('model.title'),
+            via: 'OSFramework'
+        };
+
+        return `https://twitter.com/intent/tweet?${queryStringify(queryParams)}`;
     }),
+    /* TODO: Update this with new Facebook Share Dialog, but an App ID is required
+     * https://developers.facebook.com/docs/sharing/reference/share-dialog
+     */
     facebookHref: Ember.computed('model', function() {
-        return 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(window.location.href);
+        const queryParams = {
+            u: window.location.href
+        };
+
+        return `https://www.facebook.com/sharer/sharer.php?${queryStringify(queryParams)}`;
     }),
+    // https://developer.linkedin.com/docs/share-on-linkedin
     linkedinHref: Ember.computed('model', function() {
-        return 'https://www.linkedin.com/cws/share?url=' + encodeURIComponent(window.location.href) + '&title=' + encodeURIComponent(this.get('model.title'));
+        const queryParams = {
+            url: [window.location.href, 1024],          // required
+            mini: ['true', 4],                          // required
+            title: [this.get('model.title'), 200],      // optional
+            summary: [this.get('model.abstract'), 256], // optional
+            source: ['Open Science Framework', 200]     // optional
+        };
+
+        return `https://www.linkedin.com/shareArticle?${queryStringify(queryParams)}`;
     }),
     emailHref: Ember.computed('model', function() {
-        return 'mailto:?subject=' + encodeURIComponent(this.get('model.title')) + '&body=' + encodeURIComponent(window.location.href);
+        const queryParams = {
+            subject: this.get('model.title'),
+            body: window.location.href
+        };
+
+        return `mailto:?${queryStringify(queryParams)}`;
     }),
     // The currently selected file (defaults to primary)
     activeFile: null,
