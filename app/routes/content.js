7df4e23ddf183a11ed1678ce4edf6a325db70504
Barrett K. Harber <barrett.harber@gmail.com>
2016-09-28 09:57:47 -0400

    Add open graph meta tags to content view

--- app/routes/content.js
+++ app/routes/content.js
@@ -18,5 +18,31 @@ export default Ember.Route.extend(AnalyticsMixin, ResetScrollMixin, {
             window.history.replaceState({}, 'preprints', 'preprints/' + transition.params.content.preprint_id);
             this.intermediateTransitionTo('page-not-found');
         }
+    },
+    afterModel(resolvedModel) {
+        const ogp = [
+            ['og:title', resolvedModel.get('title')],
+            ['og:image', '//osf.io/static/img/circle_logo.png'],
+            ['og:image:type', 'image/png'],
+            ['og:url', window.location.href],
+            ['og:description', resolvedModel.get('abstract')],
+            ['og:site_name', 'Open Science Framework'],
+            ['og:type', 'article']
+        ];
+
+        for (let tag of resolvedModel.get('tags'))
+            ogp.push(['article:tag', tag]);
+
+        const tags = ogp.map(item => {
+            return {
+                type: 'meta',
+                attrs: {
+                    property: item[0],
+                    content: item[1]
+                }
+            }
+        });
+
+        this.set('headTags', tags);
     }
 });
