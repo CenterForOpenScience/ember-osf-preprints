e5df4c90b3b8439eca810aebcad824e1cf9f6076
Barrett K. Harber <barrett.harber@gmail.com>
2016-09-28 13:41:47 -0400

    Fix bug when where switching languages in the browser causes errors, fix translations for popovers on submit page

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,16 +6,26 @@ import AnalyticsMixin from '../mixins/analytics-mixin';
 export default Ember.Route.extend(AnalyticsMixin, OSFAgnosticAuthRouteMixin, {
     i18n: Ember.inject.service(),
     afterModel: function() {
+        const availableLocales = this.get('i18n.locales').toArray();
         let locale;
 
+        console.log(availableLocales);
+
         // Works in Chrome and Firefox (editable in settings)
-        if (navigator.languages && navigator.languages[0])
-            locale = navigator.languages[0];
+        if (navigator.languages && navigator.languages.length) {
+            for (let lang of navigator.languages) {
+                if (availableLocales.contains(lang)) {
+                    locale = lang;
+                    break;
+                }
+            }
+        }
         // Backup for Safari (uses system settings)
-        else if (navigator.language)
+        else if (navigator.language && availableLocales.contains(navigator.language)) {
             locale = navigator.language;
+        }
 
         if (locale)
-            this.set('i18n.locale', locale);
+            this.set('i18n.locale', navigator.language);
     }
 });
