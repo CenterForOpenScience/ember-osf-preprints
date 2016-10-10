9149512c4d996bd40456e8f5e99ce1f8165c2f9a
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-10-10 12:54:59 -0400

    Add is-section-editable helper.

---
+++ app/helpers/is-section-editable.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+/**
+ * isSectionEditable helper - determines if form section can be edited.
+ *
+ * @method isSectionEditable
+ * @param {String} section form section name
+ * @return {Boolean} Is this section editable?
+ */
+export function isSectionEditable(params/*, hash*/) {
+  let section = params[0];
+  let uneditableSections= ['Submit', 'location_of_preprint'];
+  return uneditableSections.indexOf(section) === -1;
+}
+
+export default Ember.Helper.helper(isSectionEditable);
---
+++ tests/unit/helpers/is-section-editable-test.js
@@ -0,0 +1,10 @@
+import { isSectionEditable } from 'preprint-service/helpers/is-section-editable';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | is section editable');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = isSectionEditable([42]);
+  assert.ok(result);
+});
