c3943172a964235555fb64dff4a92ccd26d0dcf3
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-10-12 16:37:39 -0400

    Remove embedding of node on preprint, since have to load node separately anyway.

--- app/adapters/preprint.js
+++ app/adapters/preprint.js
@@ -2,13 +2,4 @@ import OsfAdapter from 'ember-osf/adapters/osf-adapter';
 
 // TODO: This is an OSF apiv2 endpoints, and should be moved to the ember-osf addon.
 export default OsfAdapter.extend({
-    buildURL(_, __, ___, requestType) {
-        // Embed node
-        var base = this._super(...arguments);
-        if (['createRecord', 'updateRecord', 'deleteRecord'].indexOf(requestType) === -1) {
-            return `${base}?embed=node`;
-        } else {
-            return base;
-        }
-    }
 });
