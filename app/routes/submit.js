18f4b8a34e4e79ef5b8c75d6b99628126713348f
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-10-12 15:41:12 -0400

    Set providers on controller.

--- app/routes/submit.js
+++ app/routes/submit.js
@@ -22,7 +22,11 @@ export default Ember.Route.extend(AnalyticsMixin, ResetScrollMixin, CasAuthentic
         // Fetch values required to operate the page: user and userNodes
         let userNodes = Ember.A();
 
-        var providers = this.get('store').findAll('preprint-provider');
+        this.get('store').findAll('preprint-provider')
+            .then((providers) => {
+                controller.set('providers', providers);
+            }
+        );
 
         this.get('currentUser').load()
             .then((user) => {
