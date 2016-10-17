eb3987844a75406147b16640799fb63ca7c6e9b0
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-10-17 10:33:49 -0400

    Add items missed in merge.

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -5,16 +5,16 @@ import Analytics from '../mixins/analytics';
 export default Ember.Controller.extend(Analytics, {
     fullScreenMFR: false,
     expandedAuthors: true,
-    twitterHref: Ember.computed('model', function() {
+    twitterHref: Ember.computed('node', function() {
         return encodeURI(`https://twitter.com/intent/tweet?url=${window.location.href}&text=${this.get('node.title')}&via=OSFramework`);
     }),
     facebookHref: Ember.computed('model', function() {
         return `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`;
     }),
-    linkedinHref: Ember.computed('model', function() {
-        return `https://www.linkedin.com/cws/share?url=${encodeURIComponent(window.location.href)}&title=${encodeURIComponent(this.get('model.title'))}`;
+    linkedinHref: Ember.computed('node', function() {
+        return `https://www.linkedin.com/cws/share?url=${encodeURIComponent(window.location.href)}&title=${encodeURIComponent(this.get('node.title'))}`;
     }),
-    emailHref: Ember.computed('model', function() {
+    emailHref: Ember.computed('node', function() {
         return `mailto:?subject=${encodeURIComponent(this.get('node.title'))}&body=${encodeURIComponent(window.location.href)}`;
     }),
     // The currently selected file (defaults to primary)
@@ -28,13 +28,13 @@ export default Ember.Controller.extend(Analytics, {
         return this.get('node.tags').length;
     }),
 
-    getAuthors: Ember.observer('model', function() {
-        // Cannot be called until preprint has loaded!
-        const model = this.get('model');
-        if (!model) return [];
+    getAuthors: Ember.observer('node', function() {
+        // Cannot be called until node has loaded!
+        const node = this.get('node');
+        if (!node) return [];
 
         const contributors = Ember.A();
-        loadAll(this.get('node'), 'contributors', contributors).then(() =>
+        loadAll(node, 'contributors', contributors).then(() =>
             this.set('authors', contributors)
         );
     }),
