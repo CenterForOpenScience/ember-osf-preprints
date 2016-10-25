c9f7019c26f34d89fc3deef95b3d8c3a3f331478
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-10-25 16:02:04 -0400

    Style.

--- app/routes/content.js
+++ app/routes/content.js
@@ -24,54 +24,53 @@ export default Ember.Route.extend(Analytics, ResetScrollMixin, {
         }
     },
     afterModel(preprint) {
-    return preprint.get('node').then(node => {
-        this.set('node', node);
+        return preprint.get('node').then(node => {
+            this.set('node', node);
 
-        const ogp = [
-            ['fb:app_id', config.FB_APP_ID],
-            ['og:title', node.get('title')],
-            ['og:image', '//osf.io/static/img/circle_logo.png'],
-            ['og:image:type', 'image/png'],
-            ['og:url', window.location.href],
-            ['og:description', node.get('description')],
-            ['og:site_name', 'Open Science Framework'],
-            ['og:type', 'article'],
-            ['article:published_time', new Date(preprint.get('dateCreated')).toISOString()],
-            ['article:modified_time', new Date(node.get('dateModified')).toISOString()]
-        ];
+            const ogp = [
+                ['fb:app_id', config.FB_APP_ID],
+                ['og:title', node.get('title')],
+                ['og:image', '//osf.io/static/img/circle_logo.png'],
+                ['og:image:type', 'image/png'],
+                ['og:url', window.location.href],
+                ['og:description', node.get('description')],
+                ['og:site_name', 'Open Science Framework'],
+                ['og:type', 'article'],
+                ['article:published_time', new Date(preprint.get('dateCreated')).toISOString()],
+                ['article:modified_time', new Date(node.get('dateModified')).toISOString()]
+            ];
 
-        const tags = [
-            ...preprint.get('subjects').map(subjectBlock => subjectBlock.map(subject => subject.text)),
-            ...node.get('tags')
-        ];
+            const tags = [
+                ...preprint.get('subjects').map(subjectBlock => subjectBlock.map(subject => subject.text)),
+                ...node.get('tags')
+            ];
 
-        for (let tag of tags)
-            ogp.push(['article:tag', tag]);
+            for (let tag of tags)
+                ogp.push(['article:tag', tag]);
 
-        let contributors = Ember.A();
+            let contributors = Ember.A();
 
-        loadAll(node, 'contributors', contributors).then(() => {
-            contributors.forEach(contributor => {
-                ogp.push(
-                    ['og:type', 'article:author'],
-                    ['profile:first_name', contributor.get('users.givenName')],
-                    ['profile:last_name', contributor.get('users.familyName')]
-                );
-            });
+            loadAll(node, 'contributors', contributors).then(() => {
+                contributors.forEach(contributor => {
+                    ogp.push(
+                        ['og:type', 'article:author'],
+                        ['profile:first_name', contributor.get('users.givenName')],
+                        ['profile:last_name', contributor.get('users.familyName')]
+                    );
+                });
 
-            this.set('headTags', ogp.map(item => (
-                {
-                    type: 'meta',
-                    attrs: {
-                        property: item[0],
-                        content: item[1]
+                this.set('headTags', ogp.map(item => (
+                    {
+                        type: 'meta',
+                        attrs: {
+                            property: item[0],
+                            content: item[1]
+                        }
                     }
-                }
-            )));
+                )));
 
-            this.get('headTagsService').collectHeadTags();
+                this.get('headTagsService').collectHeadTags();
+            });
         });
-    });
-
-}
+    }
 });
