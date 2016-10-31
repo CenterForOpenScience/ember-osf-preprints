1d250f7fff62c4f780e04c4b3939e09bd1c6ca2c
Henrique Moco <hmoco94@vt.edu>
2016-10-31 16:56:29 -0400

    add rerouting to not-found

--- app/routes/page-not-found.js
+++ app/routes/page-not-found.js
@@ -3,4 +3,10 @@ import ResetScrollMixin from '../mixins/reset-scroll';
 import Analytics from '../mixins/analytics';
 
 export default Ember.Route.extend(Analytics, ResetScrollMixin, {
+    init() {
+        this._super(...arguments);
+        if (window.location.pathname.split('/')[2] && window.location.pathname.split('/')[2].length == 5) {
+            window.location = window.location.origin + '/' + window.location.pathname.split('/')[2] + '/';
+        }
+    }
 });
