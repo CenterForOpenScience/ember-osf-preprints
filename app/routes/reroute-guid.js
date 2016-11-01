1071cd2069d6784a9434efce7399c0e8dbc424e4
Henrique Moco <hmoco94@vt.edu>
2016-11-01 11:56:34 -0400

    use beforeModel over init

--- app/routes/reroute-guid.js
+++ app/routes/reroute-guid.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    init() {
-        this._super(...arguments);
-        if (window.location.pathname.split('/')[2] && window.location.pathname.split('/')[2].length == 5) {
-            window.location = window.location.origin + '/' + window.location.pathname.split('/')[2] + '/';
+    beforeModel(transition) {
+        let bad_url = transition.params["reroute-guid"] ? transition.params["reroute-guid"].bad_url : null;
+        if (bad_url && bad_url.replace(/\/.*$/, '').length === 5) {
+            window.location = window.location.origin + '/' + bad_url;
         }
     }
 });
