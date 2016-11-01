eca91ec59aa7875186259d9af4adb0d7a4b7ba4f
Henrique Moco <hmoco94@vt.edu>
2016-11-01 08:59:27 -0400

    change route for redirect 2

--- app/routes/page-not-found.js
+++ app/routes/page-not-found.js
@@ -3,10 +3,4 @@ import ResetScrollMixin from '../mixins/reset-scroll';
 import Analytics from '../mixins/analytics';
 
 export default Ember.Route.extend(Analytics, ResetScrollMixin, {
-    init() {
-        this._super(...arguments);
-        if (window.location.pathname.split('/')[2] && window.location.pathname.split('/')[2].length == 5) {
-            window.location = window.location.origin + '/' + window.location.pathname.split('/')[2] + '/';
-        }
-    }
 });
--- app/routes/reroute-guid.js
+++ app/routes/reroute-guid.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    init() {
+        this._super(...arguments);
+        if (window.location.pathname.split('/')[2] && window.location.pathname.split('/')[2].length == 5) {
+            window.location = window.location.origin + '/' + window.location.pathname.split('/')[2] + '/';
+        }
+    }
 });
