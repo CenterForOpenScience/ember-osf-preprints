138cab003a6287e10dce4f7f3953e839fdd9219e
Barrett K. Harber <barrett.harber@gmail.com>
2016-11-14 18:04:27 -0500

    Fix race condition in provider route

--- app/routes/provider.js
+++ app/routes/provider.js
@@ -4,26 +4,24 @@ import config from 'ember-get-config';
 export default Ember.Route.extend({
     theme: Ember.inject.service(),
 
-    init() {
-        this.get('store')
-            .findAll('preprint-provider', { reload: true })
-            .then(data => this.set('providers', data.getEach('id')));
-    },
-
     beforeModel(transition) {
         const {slug} = transition.params.provider;
 
-        if (this.get('providers').includes(slug)) {
-            this.set('theme.id', slug);
-        } else {
-            this.set('theme.id', config.PREPRINTS.provider);
+        this.get('store')
+            .findAll('preprint-provider')
+            .then(data => {
+                if (data.getEach('id').includes(slug)) {
+                    this.set('theme.id', slug);
+                } else {
+                    this.set('theme.id', config.PREPRINTS.provider);
 
-            if (slug.length === 5) {
-                this.transitionTo('content', slug);
-            } else {
-                this.transitionTo('page-not-found');
-            }
-        }
+                    if (slug.length === 5) {
+                        this.transitionTo('content', slug);
+                    } else {
+                        this.transitionTo('page-not-found');
+                    }
+                }
+            });
     },
 
     actions: {
