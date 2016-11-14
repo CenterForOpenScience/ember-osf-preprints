228abd80570a94177cce41e4095c3717923f6e72
Barrett K. Harber <barrett.harber@gmail.com>
2016-11-14 10:16:06 -0500

    Update signup url for branded preprints (CenterForOpenScience/osf.io#6545)

--- app/services/theme.js
+++ app/services/theme.js
@@ -3,7 +3,7 @@ import config from 'ember-get-config';
 
 export default Ember.Service.extend({
     store: Ember.inject.service(),
-    id: null,
+    id: config.PREPRINTS.provider,
 
     provider: Ember.computed('id', function() {
         const id = this.get('id');
@@ -29,5 +29,13 @@ export default Ember.Service.extend({
 
         const suffix = config.ASSET_SUFFIX ? `-${config.ASSET_SUFFIX}` : '';
         return `/preprints/assets/css/${id}${suffix}.css`;
+    }),
+
+    signupUrl: Ember.computed('id', function() {
+        const query = Ember.$.param({
+            campaign: `${this.get('id')}-preprints`
+        });
+
+        return `${config.OSF.url}register?${query}`;
     }),
 });
