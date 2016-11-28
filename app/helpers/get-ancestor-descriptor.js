0d90833d0d6e3b7cb21d181c2d9d339fa40b9e3a
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-11-28 07:42:27 -0500

    Initial rewrite of get-ancestor-descriptor.

--- app/helpers/get-ancestor-descriptor.js
+++ app/helpers/get-ancestor-descriptor.js
@@ -1,42 +1,51 @@
 import Ember from 'ember';
 
-// Formats titles similar to the way they're displayed in the dashboard.  For example, Root Name / ... / Parent Name / Node Name.
+function fetchIdFromRelationshipLink(node, relationship) {
+    // Private node ids can be accessed under initializedRelationships.
+    if (node) {
+        let relationships = node._internalModel._relationships.initializedRelationships[relationship];
+        if (relationships && relationships.link) {
+            return relationships.link.split('nodes')[1].replace(/\//g, '');
+        }
+    }
+   return undefined;
+}
+
+function fetchTitle(node, relationship) {
+    // Fetches parent or root title.  If null, marks 'Private'.
+    let title = node.get(`${relationship}.title`);
+    if (typeof title === 'undefined') {
+        title = 'Private';
+    }
+    return title;
+
+}
+
 export function getAncestorDescriptor(params/*, hash*/) {
-    var node = params[0];
-    var nodeId = node.get('id');
-    var rootId = node.get('root.id');
-    var parentId = node.get('parent.id');
-    var rootDescriptor;
+    // Formats titles similar to the way they're displayed in the dashboard.  For example, Root Name / ... / Parent Name / Node Name.
+    let node = params[0];
+    let nodeId = node.get('id');
+    let rootId = node.get('root.id');
+    let parentId = node.get('parent.id')
 
-    if (typeof rootId !== 'undefined') {
-        if (typeof parentId !== 'undefined') {
-            if (parentId === rootId) {
-                rootDescriptor = node.get('root.title') + ' / ';
-            } else {
-                if (node.get('parent.parent.id') === rootId) {
-                    rootDescriptor = node.get('root.title') + ' / ' + node.get('parent.title') + ' / ';
-                } else {
-                    rootDescriptor = node.get('root.title') + ' /.../ ' + node.get('parent.title') + ' / ';
-                }
-            }
-        } else {
-            if (rootId === nodeId) {
-                rootDescriptor = '';
-            } else {
-                rootDescriptor = node.get('root.title') + '/Private/ ';
-            }
-        }
-    } else {
-        if (typeof parentId !== 'undefined') {
-            if (node.get('parent.parent.id')) {
-                rootDescriptor = 'Private / ... / ' + node.get('parent.title') + ' / ';
+    if (typeof rootId === 'undefined') rootId = fetchIdFromRelationshipLink(node, 'root');
+    if (typeof parentId === 'undefined') parentId = fetchIdFromRelationshipLink(node, 'parent');
 
-            } else {
-                rootDescriptor = 'Private / ' + node.get('parent.title') + ' / ';
-            }
-        } else {
-            rootDescriptor = '';
-        }
+    let parentTitle = fetchTitle(node, 'parent');
+    let rootTitle = fetchTitle(node, 'root');
+
+    let parent = node.get('parent').content;
+    let parentParentId = fetchIdFromRelationshipLink(parent, 'parent');
+
+    let rootDescriptor;
+    if (rootId === nodeId) { // One level
+        rootDescriptor = '';
+    } else if (rootId === parentId) { // Two levels
+        rootDescriptor = parentTitle + ' / '
+    } else if (rootId === parentParentId) { // Three levels
+        rootDescriptor = rootTitle + ' / ' + parentTitle + ' / '
+    } else { // Four + levels
+        rootDescriptor = rootTitle + ' / ... / ' + parentTitle + ' / '
     }
 
     return rootDescriptor;
