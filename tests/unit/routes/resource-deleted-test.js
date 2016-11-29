72b0415b76c8ba85e29eda4a348cf9f472aee9de
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-11-29 16:09:59 -0500

    Add resource-deleted to routes.

---
+++ tests/unit/routes/resource-deleted-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:resource-deleted', 'Unit | Route | resource deleted', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
