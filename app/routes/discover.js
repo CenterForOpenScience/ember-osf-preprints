3cfd781f047e33e446c80271d2880681aedd17c0
Henrique Moco <hmoco94@vt.edu>
2016-12-13 10:59:08 -0500

    clear the url params on link-to transition:

--- app/routes/discover.js
+++ app/routes/discover.js
@@ -14,4 +14,13 @@ export default Ember.Route.extend(Analytics, ResetScrollMixin, {
             .findAll('preprint-provider', { reload: true })
             .then(result => result.filter(item => item.id !== 'osf'));
     },
+    actions: {
+        willTransition() {
+            //Don't want to send clearFIlters action as that will trigger
+            //behavior specifc to the clearFIlters button.
+            let controller = this.controllerFor('discover');
+            controller.set('subjectFilter', '');
+            controller.set('providerFilter', '');
+        }
+    }
 });
