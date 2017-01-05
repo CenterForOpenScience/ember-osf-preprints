65a19af23766bad8521be3ee497ed1ba9e3d8560
Sherif <Sherif@Admins-MacBook-Air-2.local>
2017-01-05 10:42:24 -0500

    The branded preprint partners are using the -rXiv extension with permission from Cornell and there needs to be language on their pages stating such. - engrXiv: "arXiv is a trademark of Cornell University, used under license. This license should not be understood to indicate endorsement of content on engrXiv by Cornell University or arXiv." - SocArXiv: "arXiv is a trademark of Cornell University, used under license." - PsyArXiv: "arXiv is a trademark of Cornell University, used under license. This license should not be understood to indicate endorsement of content on PsyArXiv by Cornell University or arXiv."
    Improve preprints UI for adding the permission language to branded preprint sites:
    
    - Create a new component permission-language.
    - Create a helper function “eq” to check provider name.
    - Update the application scss.
    - Add the new component in the proper place within the larger component preprint-branded-footprint.
    
    JIRA ticket ID is EOSF-403

---
+++ tests/unit/helpers/eq-test.js
@@ -0,0 +1,12 @@
+
+import { eq } from 'preprint-service/helpers/eq';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | eq');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = eq([42]);
+  assert.ok(result);
+});
+
