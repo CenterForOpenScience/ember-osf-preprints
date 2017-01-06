3da12ba9508416c3309a7dc7710a626cf63ad1f0
Sherif <sherif@cos.io>
2017-01-06 14:15:34 -0500

    Remove integration testing for permission-language component
    Ticket ID: EOSF-403

--- tests/integration/components/permission-language-test.js
+++ tests/integration/components/permission-language-test.js
@@ -1,6 +1,5 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import Ember from 'ember';
 
 moduleForComponent('permission-language', 'Integration | Component | permission language', {
   integration: true
@@ -11,28 +10,9 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  let providerModel = Ember.Object.create({
-      engrXiv: {name:'engrXiv'},
-      SocArXiv: {name:'SocArXiv'},
-      PsyArXiv: {name:'PsyArXiv'}
-    });
+  this.render(hbs`{{permission-language}}`);
 
-  this.set('providerModel', providerModel);
-
-
-  this.render(hbs`{{permission-language model=providerModel.engrXiv}}`);
-
-  assert.equal(this.$().text().trim(), 'arXiv is a trademark of Cornell University, used under license. This license should not be understood to indicate endorsement of content on engrXiv by Cornell University or arXiv.');
-
-  this.render(hbs`{{permission-language model=providerModel.SocArXiv}}`);
-
-  assert.equal(this.$().text().trim(), 'arXiv is a trademark of Cornell University, used under license.');
-
-  this.render(hbs`{{permission-language model=providerModel.PsyArXiv}}`);
-
-  assert.equal(this.$().text().trim(), 'arXiv is a trademark of Cornell University, used under license. This license should not be understood to indicate endorsement of content on PsyArXiv by Cornell University or arXiv.');
-
-
+  assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
