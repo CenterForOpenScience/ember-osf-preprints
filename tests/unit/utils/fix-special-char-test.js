fe3b9185f1287719e16c84b1a213477c27a541fb
Sherif <sherif@cos.io>
2017-01-18 11:06:56 -0500

    Add testing to the util function fixSpecialChar

---
+++ tests/unit/utils/fix-special-char-test.js
@@ -0,0 +1,10 @@
+import fixSpecialChar from 'preprint-service/utils/fix-special-char';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | fix special char');
+
+// Replace this with your real tests.
+test('test special characters', function(assert) {
+  let result = fixSpecialChar('test special characters & >');
+  assert.equal(result,'test special characters & >');
+});
