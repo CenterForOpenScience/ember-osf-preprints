4c1b6ccbccb11d5be446bafe395c722888ec584b
Sherif <sherif@cos.io>
2017-01-19 14:10:53 -0500

    Update search filters to the latest ES mappings. Change .raw to .exact. Ticket ID: 455

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,7 +11,7 @@ export default Ember.Controller.extend(Analytics, {
         const filter = [
             {
                 term: {
-                    'types.raw': 'preprint'
+                    'types.exact': 'preprint'
                 }
             }
         ];
