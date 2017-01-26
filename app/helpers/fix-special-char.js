5ecb4437ba96238fc700019352467940ea2e4140
Sherif <sherif@cos.io>
2017-01-26 11:01:37 -0500

    Remove helper fix special char

--- app/helpers/fix-special-char.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function fixSpecialChar(params/*, hash*/) {
-  return params.join('').replace(/&amp;/g, '&');
-}
-
-export default Ember.Helper.helper(fixSpecialChar);
