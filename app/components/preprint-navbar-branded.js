d2384333ed7e23c3ac0c39c08a62b37ee19686ea
Barrett K. Harber <barrett.harber@gmail.com>
2017-01-30 16:22:39 -0500

    Add OSF host for branded navbar

--- app/components/preprint-navbar-branded.js
+++ app/components/preprint-navbar-branded.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 import OSFAgnosticAuthControllerMixin from 'ember-osf/mixins/osf-agnostic-auth-controller';
 import Analytics from '../mixins/analytics';
+import config from 'ember-get-config';
 
 export default Ember.Component.extend(OSFAgnosticAuthControllerMixin, Analytics, {
     session: Ember.inject.service(),
     theme: Ember.inject.service(),
     tagName: 'nav',
-    classNames: ['navbar', 'branded-navbar', 'preprint-navbar']
+    classNames: ['navbar', 'branded-navbar', 'preprint-navbar'],
+    host: config.OSF.url,
 });
