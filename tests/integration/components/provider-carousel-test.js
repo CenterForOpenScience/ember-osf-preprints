91b15f9e81ef516fb7eca90faacb2539bbc737cd
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-02-08 17:22:46 -0500

    Test rendering of provider carousel.

--- tests/integration/components/provider-carousel-test.js
+++ tests/integration/components/provider-carousel-test.js
@@ -12,14 +12,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{provider-carousel}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#provider-carousel}}
-      template block text
-    {{/provider-carousel}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().context.innerText, 'PreviousNext');
 });
