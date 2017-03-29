cae00413da16a4d0f86f66f7ce646bc6da25c237
Sherif Abdelhamid <sherif@cos.io>
2017-03-29 16:48:49 -0400

    Update taxonomy-top-list component

--- app/components/taxonomy-top-list.js
+++ app/components/taxonomy-top-list.js
@@ -6,7 +6,8 @@ import Analytics from '../mixins/analytics';
  */
 
 /**
- * Displays top level disciplines for preprints index page
+ * Displays top level disciplines for preprints index page. Retrieve four children subjects per each top level
+ * discipline and display as a tooltip.
  *
  * Sample usage:
  * ```handlebars
@@ -18,18 +19,35 @@ import Analytics from '../mixins/analytics';
  */
 export default Ember.Component.extend(Analytics, {
     theme: Ember.inject.service(),
-    // jscs:disable disallowQuotedKeysInObjects
-    subjectTooltips: {
-        'Architecture': 'Architectural Engineering, Construction Engineering, Environmental Design, Interior Architecture, Landscape Architecture',
-        'Arts and Humanities': 'Fine Arts, History, Music, Philosophy, Religion',
-        'Business': 'Accounting, Finance and Financial Management, Human Resource Management, Marketing, Taxation',
-        'Education': 'Curriculum and Instruction, Educational Administration and Supervision, Educational Leadership, Higher Education, Liberal Studies',
-        'Engineering': 'Biomedical Engineering and Bioengineering, Chemical Engineering, Civil and Environmental Engineering, Electrical and Computer Engineering, Mechanical Engineering',
-        'Law': 'Civil Law, Criminal Law, Legislation, State and Local Government Law, Supreme Court of the United States',
-        'Life Sciences': 'Agriculture, Genetics and Genomics, Microbiology, Physiology, Zoology',
-        'Medicine and Health Sciences': 'Anatomy, Diseases, Medical Sciences, Public Health, Veterinary Medicine',
-        'Physical Sciences and Mathematics': 'Chemistry, Computer Sciences, Earth Sciences, Physics, Statistics and Probability',
-        'Social and Behavioral Sciences': 'Anthropology, Economics, Political Science, Psychology, Sociology',
+    getSubjectChildren(parents, provider) {
+        return provider.query('taxonomies', {
+                    filter: {
+                        parents
+                    },
+                    page: {
+                        size: 4
+                    }
+                })
+            .then(results =>
+                results.map(result => result.get('text'))
+            )
+            .then(results =>
+                results.toArray().sort().join(', ')
+            );
+    },
+    getAllChildren(sortedList, i, provider) {
+        if (i >= 0) {
+            const subjectId = sortedList.objectAt(i).get('id');
+            const subjectChildren = this.getSubjectChildren(subjectId, provider);
+            subjectChildren.then(result => {
+                Ember.$('#' + subjectId).attr('data-hint', result); Ember.$('#' + subjectId).attr('aria-label', result);
+            }).then(() => this.getAllChildren(sortedList, i - 1, provider));
+        }
+    },
+    didInsertElement() {
+        this._super(...arguments);
+        const sortedList = this.get('list').sortBy('text');
+        this.get('theme.provider').then(provider => this.getAllChildren(sortedList, sortedList.get('length') - 1, provider));
     },
     // jscs:enable
     sortedList: Ember.computed('list', 'list.content', function() {
