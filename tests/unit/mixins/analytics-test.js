4c15be50bd1d5a1c485a4951da760b453f96f4e6
Sherif Abdelhamid <sherif@cos.io>
2017-04-04 21:44:09 -0400

    analytics mixin test

--- tests/unit/mixins/analytics-test.js
+++ tests/unit/mixins/analytics-test.js
@@ -1,12 +1,28 @@
 import Ember from 'ember';
 import Analytics from 'preprint-service/mixins/analytics';
-import { module, test } from 'qunit';
+import { moduleFor, test } from 'ember-qunit';
 
-module('Unit | Mixin | analytics');
+moduleFor('mixin:analytics', {
+    needs: [
+        'service:metrics',
+    ],
+});
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let AnalyticsMixinObject = Ember.Object.extend(Analytics);
-  let subject = AnalyticsMixinObject.create();
-  assert.ok(subject);
+test("Google Analytics mixin", function(assert) {
+    let analyticsObject = Ember.Controller.extend(Analytics);
+    this.registry.register('test:subject', analyticsObject);
+    const subject = this.container.lookup('test:subject');
+    assert.ok(subject);
+    const checkActions = (subject.get('actions.click') && subject.get('actions.track'));
+    if (checkActions) {
+        assert.ok(true);
+    } else {
+        assert.ok(false);
+    }
+    const checkMetrics = (subject.get('metrics'));
+    if (checkMetrics) {
+        assert.ok(true);
+    } else {
+        assert.ok(false);
+    }
 });
