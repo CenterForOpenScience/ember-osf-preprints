604a1db126da40f96e68675efe9dfd830e01947f
Baylee Swenson <baylee@cos.io>
2017-06-05 16:58:47 -0400

    Added test coverage

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -9,3 +9,19 @@ test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
 });
+
+test('google metrics array exists', function(assert) {
+    // Tests that there are google metrics tags included on the page
+    let component = this.subject();
+    
+    component.set('googleScholarTags', [{
+            users: {
+            givenName: 'Tester',
+            familyName: 'Test',
+            fullName: 'Tester J. Test'
+            }
+        }
+    ]);
+
+    assert.ok(component.get('googleScholarTags').length);
+});
