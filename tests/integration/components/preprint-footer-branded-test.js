68b3222e37e3d97379cc61e7bf412dd90c06b127
Lauren Revere <laurenrevere@cos.io>
2017-06-13 09:28:25 -0400

    Update preprint template with new fields: description, footerLinks, advisoryBoard
    Delete name check test
    
    Fix footer tests

--- tests/integration/components/preprint-footer-branded-test.js
+++ tests/integration/components/preprint-footer-branded-test.js
@@ -5,47 +5,9 @@ moduleForComponent('preprint-footer-branded', 'Integration | Component | preprin
   integration: true
 });
 
-test('shows Twitter', function(assert) {
-    this.set('model', {
-        socialTwitter: 'vincestaples'
-    });
-
-    this.render(hbs`{{preprint-footer-branded model=model}}`);
-
-    assert.equal(this.$('.fa-twitter').length, 1);
-    assert.equal(this.$('.fa-facebook').length, 0);
-    assert.equal(this.$('.fa-instagram').length, 0);
-});
-
-test('shows Facebook', function(assert) {
-    this.set('model', {
-        socialFacebook: 'theWeatherStn'
-    });
-
-    this.render(hbs`{{preprint-footer-branded model=model}}`);
-
-    assert.equal(this.$('.fa-twitter').length, 0);
-    assert.equal(this.$('.fa-facebook').length, 1);
-    assert.equal(this.$('.fa-instagram').length, 0);
-});
-
-test('shows Instagram', function(assert) {
-    this.set('model', {
-        socialInstagram: 'welpherewego'
-    });
-
-    this.render(hbs`{{preprint-footer-branded model=model}}`);
-
-    assert.equal(this.$('.fa-twitter').length, 0);
-    assert.equal(this.$('.fa-facebook').length, 0);
-    assert.equal(this.$('.fa-instagram').length, 1);
-});
-
 test('shows all social', function(assert) {
     this.set('model', {
-        socialTwitter: 'vincestaples',
-        socialFacebook: 'theWeatherStn',
-        socialInstagram: 'welpherewego'
+        footerLinks: '<i class="fa fa-2x fa-twitter"></i><i class="fa fa-2x fa-instagram"></i><i class="fa fa-2x fa-facebook"></i>'
     });
 
     this.render(hbs`{{preprint-footer-branded model=model}}`);
@@ -53,13 +15,4 @@ test('shows all social', function(assert) {
     assert.equal(this.$('.fa-twitter').length, 1);
     assert.equal(this.$('.fa-facebook').length, 1);
     assert.equal(this.$('.fa-instagram').length, 1);
-});
-
-test('uses provider name', function(assert) {
-    const name = 'Social Experiment';
-    this.set('model', { name });
-
-    this.render(hbs`{{preprint-footer-branded model=model}}`);
-
-    assert.ok(this.$(`.branded-footer-links:contains('${name}')`).length);
 });
