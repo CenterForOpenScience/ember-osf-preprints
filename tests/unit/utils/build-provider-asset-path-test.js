4ca56946ccc518ce57188ef64e9327f97f6f30be
James C. Davis <james@cos.io>
2017-06-15 17:30:41 -0400

    Update buildProviderAssetPath unit test to do something useful

--- tests/unit/utils/build-provider-asset-path-test.js
+++ tests/unit/utils/build-provider-asset-path-test.js
@@ -1,10 +1,15 @@
+import config from 'ember-get-config';
 import buildProviderAssetPath from 'preprint-service/utils/build-provider-asset-path';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | build provider asset path');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = buildProviderAssetPath();
-  assert.ok(result);
+test('build correct path for domain', function(assert) {
+    let result = buildProviderAssetPath('foo', 'bar.baz', true);
+    assert.equal(result, `${config.providerAssetsURL}foo/bar.baz`);
+});
+
+test('build correct path for non-domain', function(assert) {
+    let result = buildProviderAssetPath('foo', 'bar.baz', false);
+    assert.equal(result, `${config.providerAssetsURL}foo/bar.baz`);
 });
