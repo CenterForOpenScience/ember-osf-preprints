2be9ff27041d2d991b9bcbdd0263e5773378dbbd
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-2.local>
2018-01-08 16:27:22 -0500

    rename review action for resubmit

--- app/controllers/submit.js
+++ app/controllers/submit.js
@@ -1078,7 +1078,7 @@ export default Ember.Controller.extend(Analytics, BasicsValidations, NodeActions
             this.set('savingPreprint', true);
             this.toggleProperty('shareButtonDisabled');
 
-            let submitAction = this.get('store').createRecord('action', {
+            let submitAction = this.get('store').createRecord('review-action', {
                 actionTrigger: 'submit',
                 target: this.get('model')
             });
