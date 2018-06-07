e9248508fec2205c471be2a601f54fd29923d2da
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-4.local>
2018-06-07 17:33:28 -0400

    try to fix tests

--- tests/integration/components/preprint-navbar-branded-test.js
+++ tests/integration/components/preprint-navbar-branded-test.js
@@ -1,8 +1,23 @@
-import { moduleForComponent, skip } from 'ember-qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 import Service from '@ember/service';
 import hbs from 'htmlbars-inline-precompile';
 
 import tHelper from 'ember-i18n/helper';
+
+const fakeProvider = {
+    id: 'pandaXriv',
+    name: 'The Panda Archive of bamboo',
+    isProvider: true,
+};
+
+// Stub theme service
+const themeStub = Service.extend({
+    provider: fakeProvider,
+    signupUrl: 'fakeUrl',
+    redirectUrl: 'fakeUrl',
+    pathPrefix: 'fakePrefix',
+    id: 'pandaXriv',
+});
 
 // Stub i18n service
 const i18nStub = Service.extend({
@@ -22,10 +37,11 @@ moduleForComponent('preprint-navbar-branded', 'Integration | Component | preprin
     beforeEach() {
         this.registry.register('helper:t', tHelper);
         this.register('service:i18n', i18nStub);
+        this.register('service:theme', themeStub);
     },
 });
 
-skip('renders preprint navbar branded', function(assert) {
+test('renders preprint navbar branded', function(assert) {
     this.render(hbs`{{preprint-navbar-branded}}`);
     assert.equal(this.$().text().replace(/\s+/g, ' ').trim(), 'Search Donate Sign Up Sign In');
 });
