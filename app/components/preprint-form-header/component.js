bbe3a4b9e28b1b42f152b7bba04f713cc3b7851a
Lauren Barker <lauren@cos.io>
2018-07-31 17:08:27 -0400

    Fix validation css

--- app/components/preprint-form-header/component.js
+++ app/components/preprint-form-header/component.js
@@ -1,14 +1,12 @@
-import { computed } from '@ember/object';
-import config from 'ember-get-config';
-import CpPanelToggleComponent from 'ember-collapsible-panel/components/cp-panel-toggle';
+// import { computed } from '@ember/object';
+import Component from '@ember/component';
 
 /**
- * @module ember-preprints
+ * @module ember-osf-preprints
  * @submodule components
  */
 
 /**
- * Extends Ember Collapsible Panel's CpPanelToggleComponent.
  * Customizes the collapsible panel header.
  *
  * Sample usage:
@@ -23,25 +21,26 @@ import CpPanelToggleComponent from 'ember-collapsible-panel/components/cp-panel-
  * ```
  * @class preprint-form-header
  * */
-export default CpPanelToggleComponent.extend({
-    tagName: 'header',
-    // Variables to pass in
-    enabled: true,
-    valid: null,
-    isValidationActive: false,
+export default Component.extend({
+    tagName: '',
+
     // CSS controls icon color and display.
     // If neither valid nor invalid state applies, don't show icon.
-    classNameBindings: ['enabled::disabled', 'valid:valid', 'invalid:invalid', 'isValidationActive::not-validated'],
+    // classNameBindings: ['valid:valid', 'invalid:invalid'],
+
+    // Variables to pass in
+    valid: null,
+    // isValidationActive: false,
+
     // Calculated properties
-    invalid: computed('valid', 'isValidationActive', function() {
-        // If the user hasn't even opened the panel yet, don't run the validation check
-        // In other words, not true or null
-        if (this.get('isValidationActive')) {
-            return !this.get('valid');
-        } else {
-            return false;
-        }
-    }),
-    providerAssetsURL: config.providerAssetsURL,
+    // invalid: computed('valid', 'isValidationActive', function() {
+    //     // If the user hasn't even opened the panel yet, don't run the validation check
+    //     // In other words, not true or null
+    //     if (this.get('isValidationActive')) {
+    //         return !this.get('valid');
+    //     } else {
+    //         return false;
+    //     }
+    // }),
 
 });
