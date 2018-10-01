53bb3a260babf8fb8910c537b5ad45ca03509b6b
Yuhuai Liu <yuhuai@cos.io>
2018-10-01 13:07:04 -0400

    minor fixes

--- app/components/chronos-submission-status-list-row/component.js
+++ app/components/chronos-submission-status-list-row/component.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-// import { task } from 'ember-concurrency';
 
 const DRAFTED = 'DRAFT';
 const SUBMITTED = 'SUBMITTED';
