892924043961423965ee9d1a16abfa731da2b495
Futa Ikeda <futa.ikeda@gmail.com>
2020-03-20 14:15:49 -0400

    Add component to handle multiple text inputs

---
+++ app/components/multiple-textbox-input.js
@@ -0,0 +1,37 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { task, timeout } from 'ember-concurrency';
+import Analytics from 'ember-osf/mixins/analytics';
+
+export default Component.extend(Analytics, {
+    textFields: null, // passed in array of text fields on invocation, default to null
+    alphabet: 'abcdefghijklmnop',
+    textFieldsLastIndex: computed('textFields.[]', function() {
+        return this.get('textFields').length - 1;
+    }),
+
+    actions: {
+        addTextField() {
+            let fields = this.get('textFields');
+            if (!fields) {
+                fields = [$('.ember-text-field')[0].value];
+            }
+            fields.pushObject('');
+            this.set('textFields', fields);
+        },
+        removeTextField(index) {
+            const fields = this.get('textFields');
+            fields.removeAt(index);
+            this.set('textFields', fields);
+        },
+    },
+    onFieldChange: task(function* (index) {
+        yield timeout(200); // debounce
+        let fields = this.get('textFields');
+        if (!fields) {
+            fields = [$('.ember-text-field')[0].value];
+        }
+        fields[index] = $('.ember-text-field')[index].value;
+        this.set('textFields', fields);
+    }).restartable(),
+});
---
+++ tests/integration/components/multiple-textbox-input-test.js
@@ -0,0 +1,39 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('multiple-textbox-input', 'Integration | Component | multiple textbox input', {
+    integration: true,
+    beforeEach() {
+        this.set('textFields', ['ichi', 'ni', 'san', 'yon']);
+    },
+});
+
+test('it renders', function(assert) {
+    this.render(hbs`{{multiple-textbox-input}}`);
+    assert.equal($('.ember-text-field').length, 1, 'one text field by default');
+    assert.equal($('.btn-danger').length, 0, 'no remove button for single text field');
+    assert.equal($('.btn-success').length, 1, 'one add button');
+});
+
+test('it loads passed in fields', function(assert) {
+    this.render(hbs`{{multiple-textbox-input textFields=textFields}}`);
+    assert.equal($('.ember-text-field').length, 4, 'has four text fields when passed in an array of four');
+    assert.equal($('.btn-danger').length, 3, 'has three remove buttons when passed in array of four');
+    assert.equal($('.btn-success').length, 1, 'one add button');
+});
+
+test('Can add and remove fields', function(assert) {
+    this.render(hbs`{{multiple-textbox-input textFields=textFields}}`);
+    this.$('.btn-danger')[0].click();
+    assert.equal($('.ember-text-field').length, 3, 'has three text fields after single remove');
+    assert.equal($('.btn-danger').length, 2, 'has two remove buttons after single remove');
+    assert.equal($('.ember-text-field')[0].value, "ni", 'old index 1 now index 0 after single remove');
+    assert.equal($('.ember-text-field')[2].value, "yon", 'old index 3 now index 2 after single remove');
+    this.$('.btn-success')[0].click();
+    assert.equal($('.ember-text-field').length, 4, 'has 4 text fields after single add');
+    assert.equal($('.btn-danger').length, 3, 'has three remove buttons after single remove');
+    assert.equal($('.ember-text-field')[0].value, "ni", 'existing index 0 unchanged by add');
+    assert.equal($('.ember-text-field')[2].value, "yon", 'existing index 2 unchanged by add');
+    assert.equal($('.ember-text-field')[3].value, "", 'new text field is empty');
+    assert.equal($('.btn-success').length, 1);
+})
