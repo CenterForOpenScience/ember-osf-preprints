afa04fac7acb070be5c97bd6a8030dab2023e25a
Futa Ikeda <futa.ikeda@gmail.com>
2020-09-15 12:11:06 -0400

    Use query instead of findAll

--- app/controllers/submit.js
+++ app/controllers/submit.js
@@ -1481,7 +1481,7 @@ export default Controller.extend(Analytics, BasicsValidations, COIValidations, N
 
     _setCurrentProvider() {
         this.get('store')
-            .findAll('preprint-provider', { adapterOptions: { query: { 'filter[allow_submissions]': true } }, reload: true })
+            .query('preprint-provider', { filter: { allow_submissions: true } })
             .then(this._getProviders.bind(this));
     },
 
