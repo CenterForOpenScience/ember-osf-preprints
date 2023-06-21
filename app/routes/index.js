dcd7401a17b752e140926e3f5cf12aa9d4feaf0e
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2023-06-21 14:50:11 -0400

    Revert "Reroute users to new search page (#748)"
    This reverts commit 07c1982c157d812997d07550667fcf53e8505f81.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,6 @@
 import RSVP from 'rsvp';
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
-import config from 'ember-get-config';
 import Analytics from 'ember-osf/mixins/analytics';
 
 import ResetScrollMixin from '../mixins/reset-scroll';
@@ -36,15 +35,11 @@ export default Route.extend(Analytics, ResetScrollMixin, {
     },
     actions: {
         search(q) {
-            if (this.get('theme.isSubRoute')) {
-                // TODO Phase2 search improvement: reroute users to new branded preprint discover page
-                const route = 'provider.discover';
-                this.transitionTo(route, { queryParams: { q } });
-            } else {
-                // If OSF, reroute to new search page
-                window.location.href = `${this.host}/search?q=${q}&resourceType=osf:Preprints`;
-            }
+            let route = 'discover';
+
+            if (this.get('theme.isSubRoute')) { route = `provider.${route}`; }
+
+            this.transitionTo(route, { queryParams: { q } });
         },
     },
-    host: config.OSF.url,
 });
