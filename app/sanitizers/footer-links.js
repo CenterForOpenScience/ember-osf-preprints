868c4756cd8c86eb18d713a0c1dfaf4e5aec6154
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2023-06-23 11:39:14 -0400

    [No ticket] Remove bower (#751)
    ## Purpose
    - Remove bower and hopefully have stuff build on jenkins
    
    ## Summary of Changes/Side Effects
    - Removed bower and references to it
    - Added remaining dependencies in bower.json to package.json
    - Remove ember-sanitize as it is still using bower in favor of ember-cli-string-helpers

--- app/sanitizers/footer-links.js
+++
@@ -1,5 +0,0 @@
-// For bleaching preprint provider attribute footerLinks
-export default {
-    elements: ['a', 'br', 'div', 'em', 'p', 'span', 'strong', 'b', 'i', 'u'],
-    attributes: { __ALL__: ['class', 'style', 'href', 'title', 'target'] },
-};
